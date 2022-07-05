e94c4e8487c1022f4a89d438967d71984573119c
Ao Sakurai <ao.sakurai@acompany-ac.com>
2022-07-05 11:31:09 +0000

    fix: spdlog/fmt/ostr.hの順序がclang-formatに変更されないようにアノテーション付与

--- src/ComputationContainer/LogHeader/Logger.hpp
+++ src/ComputationContainer/LogHeader/Logger.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
-#include "spdlog/fmt/ostr.h"
 #include "spdlog/sinks/basic_file_sink.h"
 #include "spdlog/spdlog.h"
+//clang-format off
+#include "spdlog/fmt/ostr.h"
+//clang-format on
