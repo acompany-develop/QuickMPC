ac928292bbf7f9e055d25d32c484a3d3cebe5f44
Shingo Ohtsuka <capriciouswalk@gmail.com>
2022-09-04 13:23:12 +0000

    Fix build target list
    remove target which use pre-build image

--- Test/ManageContainer/mc_debug.sh
+++ Test/ManageContainer/mc_debug.sh
@@ -1,5 +1,5 @@
 build() {
-    docker buildx bake -f docker-compose.yml dev_mc1 dev_mc_envoy1 dev_mc2 dev_mc_envoy2 dev_mc3 dev_mc_envoy3 dev_cc1 dev_dbgate1 dev_bts dev_sharedb1 dev_secrets-server dev_cc2 dev_dbgate2 dev_sharedb2 dev_cc3 dev_dbgate3 dev_sharedb3 --load
+    docker buildx bake -f docker-compose.yml dev_mc1 dev_mc2 dev_mc3 dev_cc1 dev_dbgate1 dev_bts dev_sharedb1 dev_secrets-server dev_cc2 dev_dbgate2 dev_sharedb2 dev_cc3 dev_dbgate3 dev_sharedb3 --load
 }
 
 setup() {
