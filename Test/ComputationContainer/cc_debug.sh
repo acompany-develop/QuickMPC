2685a496e8de76630feb9b50e8042890a53fcfc8
Nakata <ryosuke.nakata@acompany-ac.com>
2022-10-14 15:41:02 +0900

    Remove dbgate from Test

--- Test/ComputationContainer/cc_debug.sh
+++ Test/ComputationContainer/cc_debug.sh
@@ -1,6 +1,6 @@
 # src/<path>/*_test.*の実行方法をTest/<path>/test.shに記述する
 build() {
-    docker-compose -f docker-compose.yml build dev_cc1 dev_cc2 dev_cc3 dev_dbgate1 dev_bts dev_sharedb1 dev_secrets-server dev_dbgate2 dev_sharedb2 dev_dbgate3 dev_sharedb3
+    docker-compose -f docker-compose.yml build dev_cc1 dev_cc2 dev_cc3 dev_bts
 }
 
 setup() {
