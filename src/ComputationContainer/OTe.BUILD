35cad463d2687667a1930e34e9c260401a69acce
watason <wellwantwater@hotmail.co.jp>
2022-10-17 02:03:52 +0000

    Add: OTLib env

---
+++ src/ComputationContainer/OTe.BUILD
@@ -0,0 +1,9 @@
+licenses(["notice"])  # ISC (簡略化されたBSDおよびMITライセンスと機能的に同等)
+
+exports_files(["LICENSE.ISC"])
+
+cc_library(
+    name = "OTe",
+    srcs = ["liblibOTe.a"],
+    visibility = ["//visibility:public"],
+)
--- src/ComputationContainer/Share/Compare.hpp
+++ src/ComputationContainer/Share/Compare.hpp
@@ -6,6 +6,8 @@
 #include "FixedPoint/FixedPoint.hpp"
 #include "Networking.hpp"
 #include "Share.hpp"
+#include "libOTe/NChooseOne/Oos/OosNcoOtReceiver.h"
+#include "libOTe/NChooseOne/Oos/OosNcoOtSender.h"
 namespace qmpc::Share
 {
 
