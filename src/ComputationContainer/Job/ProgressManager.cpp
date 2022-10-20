7355fa935104931063c1cb0bac66dbb53c3ade45
Shingo Ohtsuka <capriciouswalk@gmail.com>
2022-10-20 07:45:26 +0000

    WIP: implement ProgressManager

--- src/ComputationContainer/Job/ProgressManager.cpp
+++ src/ComputationContainer/Job/ProgressManager.cpp
@@ -2,20 +2,22 @@
 
 #include <atomic>
 #include <chrono>
+#include <memory>
+#include <sstream>
 
 #include "LogHeader/Logger.hpp"
+#include "external/Proto/common_types/common_types.pb.h"
 
 namespace qmpc::Job
 {
-class ProgressManager::Impl
+class Observer
 {
 public:
-    using process_name_type = std::pair<int, std::string>;
+    using process_name_type = std::pair<std::size_t, std::string>;
 
-    void push(const std::string& desc, const std::shared_ptr<Progress> elem)
+    void push(const std::shared_ptr<Progress> elem)
     {
-        const process_name_type::first_type id = counter++;
-        const process_name_type key{id, desc};
+        const process_name_type key{elem->id(), elem->description()};
         {
             std::lock_guard<std::mutex> lock(mutex);
             progress[key] = elem;
@@ -31,15 +33,52 @@ public:
         }();
         for (const auto& [key, value] : progress)
         {
-            spdlog::info("{} {}: {}", key.first, key.second, value->progress());
+            spdlog::debug("[PROGRESS] {} {}: {}", key.first, key.second, value->progress());
         }
     }
+
+    void finishProgress(const Progress& elem)
+    {
+        // TODO
+        const process_name_type key{elem.id(), elem.description()};
+        spdlog::debug("[PROGRESS] {} {} : finished", key.first, key.second);
+    }
+
+    std::size_t generateId() { return counter++; }
+
+    void updateJobStatus(const pb_common_types::JobStatus& status) { this->status = status; }
 
 private:
     std::mutex mutex;
     std::map<process_name_type, std::shared_ptr<Progress>> progress;
+    std::atomic<pb_common_types::JobStatus> status;
     std::atomic<process_name_type::first_type> counter{0};
 };
+
+Progress::Progress(const Builder& builder)
+    : Progress(builder.id, builder.description, builder.observer)
+{
+}
+Progress::Progress(std::size_t id, std::string description, std::shared_ptr<Observer> observer)
+    : id_(id), description_(description), observer_(observer)
+{
+}
+
+void Progress::finish() { observer_->finishProgress(*this); }
+const std::size_t& Progress::id() const { return id_; }
+const std::string& Progress::description() const { return description_; }
+
+ProgressIters::ProgressIters(const Progress::Builder& builder, const std::size_t& size)
+    : Progress(builder), size(size), index(0)
+{
+}
+void ProgressIters::update(const std::size_t& index) { this->index = index; }
+std::string ProgressIters::progress() const
+{
+    std::ostringstream os;
+    os << index << "/" << size;
+    return os.str();
+}
 
 void ProgressManager::log()
 {
@@ -51,8 +90,9 @@ void ProgressManager::log()
 
     for (const auto& [key, value] : progresses)
     {
-        spdlog::info("{}", key);
+        spdlog::debug("[PROGRESS] {} -- start", key);
         value->show();
+        spdlog::debug("[PROGRESS] {} -- end", key);
     }
 }
 
@@ -78,18 +118,42 @@ void ProgressManager::registerJob(const int& id, const std::string& uuid)
 {
     std::lock_guard<std::mutex> lock(dict_mtx);
 
-    dict[id] = uuid;
-    progresses[uuid] = std::make_shared<Impl>();
+    job_id_to_job_uuid[id] = uuid;
+    job_uuid_to_job_id[uuid] = id;
+    progresses[uuid] = std::make_shared<Observer>();
 }
 
-void ProgressManager::push(const int& id, const std::string& desc, std::shared_ptr<Progress> elem)
+std::shared_ptr<Observer> ProgressManager::getObserver(const int& id)
 {
-    const auto impl = [&]()
+    std::lock_guard<std::mutex> lock(dict_mtx);
+    return progresses[job_id_to_job_uuid[id]];
+}
+
+std::size_t ProgressManager::getProgressId(std::shared_ptr<Observer> observer)
+{
+    return observer->generateId();
+}
+
+void ProgressManager::push(std::shared_ptr<Observer> observer, std::shared_ptr<Progress> elem)
+{
+    observer->push(elem);
+}
+
+void ProgressManager::updateJobStatus(
+    const std::string& job_uuid, const pb_common_types::JobStatus& status
+)
+{
+    std::lock_guard<std::mutex> lock(dict_mtx);
+    auto observer = progresses[job_uuid];
+    observer->updateJobStatus(status);
+
+    if (status == pb_common_types::JobStatus::COMPLETED)
     {
-        std::lock_guard<std::mutex> lock(dict_mtx);
-        return progresses[dict[id]];
-    }();
-    impl->push(desc, elem);
+        progresses.erase(job_uuid);
+        auto job_id = job_uuid_to_job_id[job_uuid];
+        job_id_to_job_uuid.erase(job_id);
+        job_uuid_to_job_id.erase(job_uuid);
+    }
 }
 
 std::shared_ptr<ProgressManager> ProgressManager::getInstance()
@@ -99,6 +163,7 @@ std::shared_ptr<ProgressManager> ProgressManager::getInstance()
 }
 void ProgressManager::runProgressManager()
 {
+    spdlog::set_level(spdlog::level::debug);
     auto progress_manager = getInstance();
     spdlog::info("Progress Manager Start");
 
--- src/ComputationContainer/Job/ProgressManager.hpp
+++ src/ComputationContainer/Job/ProgressManager.hpp
@@ -5,17 +5,61 @@
 #include <map>
 #include <memory>
 #include <mutex>
+#include <string>
+
+#include "common_types/common_types.pb.h"
 
 namespace qmpc::Job
 {
+class Observer;
+
 struct Progress
 {
-    virtual std::string progress() const;
+    struct Builder
+    {
+        Builder() {}
+        Builder& setId(const std::size_t& id)
+        {
+            this->id = id;
+            return *this;
+        }
+        Builder& setDesc(const std::string& desc)
+        {
+            this->description = desc;
+            return *this;
+        }
+        Builder& setObserver(const std::shared_ptr<Observer> observer)
+        {
+            this->observer = observer;
+            return *this;
+        }
+
+        friend Progress;
+
+    private:
+        std::size_t id;
+        std::string description;
+        std::shared_ptr<Observer> observer;
+    };
+
+    Progress() = delete;
+    Progress(const Builder& builder);
+    virtual std::string progress() const = 0;
+
+    virtual void finish() final;
+    virtual const std::size_t& id() const final;
+    virtual const std::string& description() const final;
+
+private:
+    Progress(std::size_t id, std::string description, std::shared_ptr<Observer> observer);
+    std::size_t id_;
+    std::string description_;
+    std::shared_ptr<Observer> observer_;
 };
 
 struct ProgressIters : public Progress
 {
-    ProgressIters(const std::size_t& size);
+    ProgressIters(const Progress::Builder& builder, const std::size_t& size);
 
     void update(const std::size_t& index);
 
@@ -33,14 +77,15 @@ class ProgressManager
     bool shutdown_flag;
 
     std::mutex dict_mtx;
-    std::map<int, std::string> dict;
-
-    class Impl;
-    std::map<std::string, std::shared_ptr<Impl>> progresses;
+    std::map<int, std::string> job_id_to_job_uuid;
+    std::map<std::string, int> job_uuid_to_job_id;
+    std::map<std::string, std::shared_ptr<Observer>> progresses;
 
     void run();
     void log();
-    void push(const int& id, const std::string& desc, std::shared_ptr<Progress> elem);
+    std::shared_ptr<Observer> getObserver(const int& id);
+    std::size_t getProgressId(std::shared_ptr<Observer> observer);
+    void push(std::shared_ptr<Observer> observer, std::shared_ptr<Progress> elem);
 
 public:
     static std::shared_ptr<ProgressManager> getInstance();
@@ -48,13 +93,22 @@ public:
     void shutdown();
     void registerJob(const int& id, const std::string& uuid);
 
-    template <typename progress_type, class... Args>
+    template <class progress_type, class... Args>
     std::shared_ptr<progress_type> createProgress(
-        const int& id, const std::string& desc, Args&&... args
+        const int& job_id, const std::string& desc, Args&&... args
     )
     {
-        const auto progress = std::make_shared<progress_type>(args...);
-        push(id, desc, progress);
+        auto observer = getObserver(job_id);
+        auto progress_id = getProgressId(observer);
+        auto builder = Progress::Builder{}.setId(progress_id).setDesc(desc).setObserver(observer);
+
+        const auto progress = std::make_shared<progress_type>(builder, args...);
+
+        push(observer, progress);
+
+        return progress;
     }
+
+    void updateJobStatus(const std::string& job_uuid, const pb_common_types::JobStatus& status);
 };
 }  // namespace qmpc::Job
