7dc254d649114307309b6bb797e45053d020be5f
Nakata <ryosuke.nakata@acompany-ac.com>
2022-11-07 19:17:18 +0900

    Update variance test

--- Test/LibClient/src/tests/test_variance.py
+++ Test/LibClient/src/tests/test_variance.py
@@ -1,42 +1,50 @@
 import math
 
-import numpy as np
 import pytest
 from utils import get_result, qmpc
 
-from .common import send_share
+from tests.common import data_id
+
+
+def execute_computation_param(dataIds=[data_id([[1, 2, 3], [4, 7, 10]])],
+                              join=[],
+                              index=[1],
+                              src=[1, 2, 3]):
+    return ((dataIds, join, index), src)
 
 
 @pytest.mark.parametrize(
-    ("table_file"),
+    ("param", "expected"),
     [
-        # NOTE: テスト時間が長くなるためテストケースを一部省略
-        ("table_data_5x5"),
-        # ("table_data_10x10"),
-        # ("table_data_100x10"),
-        # ("table_data_1000x10"),
-        # ("table_data_10000x10"),
-        ("table_data_100x100"),
+        # usually case
+        (execute_computation_param(),
+         [2.25, 6.25, 12.25]),
+        # small table size case
+        (execute_computation_param(dataIds=[data_id([[1]])],
+                                   src=[1]),
+         [0]),
+        # large data case
+        (execute_computation_param(dataIds=[data_id([[10**9], [10**9+10]])],
+                                   src=[1]),
+            [25]),
+        # duplicated src case
+        (execute_computation_param(src=[1, 2, 2, 3, 3, 3]),
+         [2.25, 6.25, 6.25, 12.25, 12.25, 12.25]),
+        # empty src case
+        (execute_computation_param(src=[]),
+         []),
+        # variance is 0 case
+        (execute_computation_param(dataIds=[data_id([[1], [1], [1]])],
+                                   src=[1]),
+         [0]),
     ]
 )
-def test_variance(table_file: str):
+def test_sum_variance(param: tuple, expected: list):
 
-    # share送信
-    secrets, data_id = send_share(f"Data/{table_file}.csv")
-
-    # table情報と列指定情報を定義して計算
-    length = len(secrets[0])
-    table = [[data_id], [], [1]]
-    inp = [i for i in range(2, length+1)]
-
-    res = get_result(qmpc.variance(table, inp))
+    # 総和計算リクエスト送信
+    res = get_result(qmpc.variance(*param))
     assert (res["is_ok"])
 
     # 正しく計算されたか確認
-    secrets_np = np.array(secrets)[:, 1:]
-    true_val = np.var(secrets_np, axis=0)
-    for x, y in zip(res["results"], true_val):
+    for x, y in zip(res["results"], expected):
         assert (math.isclose(x, y, abs_tol=0.1))
-
-    # 冪等性のために消しておく
-    qmpc.delete_share([data_id])
