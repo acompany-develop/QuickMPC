82f450f2759b73ac0dd3a6c5c6eaa623083db9a5
Nakata <ryosuke.nakata@acompany-ac.com>
2022-11-09 15:25:34 +0900

    Update linear regresiion predict test

--- Test/LibClient/src/tests/common.py
+++ Test/LibClient/src/tests/common.py
@@ -17,3 +17,9 @@ def data_id(secrets, schema=None):
     res_send = qmpc.send_share(secrets, schema)
     assert (res_send["is_ok"])
     return res_send["data_id"]
+
+
+def job_uuid(secrets, schema=None):
+    res_send = qmpc.send_model_params(secrets)
+    assert (res_send["is_ok"])
+    return res_send["job_uuid"]
