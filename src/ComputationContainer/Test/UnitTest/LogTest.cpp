4664be7fef94b9dce43cca8f7a84d92c1bee6f0c
Shingo Ohtsuka <capriciouswalk@gmail.com>
2022-11-10 05:00:43 +0000

    Fix Logger unit tests

--- src/ComputationContainer/Test/UnitTest/LogTest.cpp
+++ src/ComputationContainer/Test/UnitTest/LogTest.cpp
@@ -1,24 +1,38 @@
+#include <iostream>
+#include <sstream>
 #include <vector>
 
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/trim.hpp>
+
 #include "Logging/Logger.hpp"
+#include "spdlog/sinks/ostream_sink.h"
+
 #include "gtest/gtest.h"
 #include "gtest/internal/gtest-port.h"
-template <typename Stream, typename Func>
-std::vector<std::string> consoleDebug(Stream &&s, Func &&f)
-{
-    std::stringstream ss;
-    std::streambuf *buffer = s.rdbuf();
-    s.rdbuf(ss.rdbuf());
-    f();
-    s.rdbuf(buffer);
 
-    std::string str;
+template <typename Func>
+std::vector<std::string> consoleDebug(Func &&f)
+{
+    std::ostringstream oss;
+    auto oss_sink = std::make_shared<spdlog::sinks::ostream_sink_mt>(oss);
+
+    qmpc::Log::addSink(oss_sink);
+
+    f();
+
+    qmpc::Log::removeSink(oss_sink);
+
+    std::string output(oss.str());
     std::vector<std::string> testVec;
-    char delim = '|';
-    while (std::getline(ss, str, delim))
+    boost::algorithm::split(testVec, output, boost::is_any_of("-|"));
+
+    for (auto &msg : testVec)
     {
-        testVec.push_back(str);
+        boost::algorithm::trim(msg);
     }
+
     return testVec;
 }
 TEST(LogTest, InfoTest)
@@ -27,28 +41,41 @@ TEST(LogTest, InfoTest)
     double b = 3.4;
     std::string message = "info log";
 
-    auto testVec = consoleDebug(std::cout, [=]() { qmpc::Log::Info(message, a, b); });
-    EXPECT_EQ("INFO", testVec[1]);
-    EXPECT_EQ("info log", testVec[2]);
-    EXPECT_EQ("5", testVec[3]);
-    EXPECT_EQ("3.4\n", testVec[4]);
+    auto testVec = consoleDebug([=]() { QMPC_LOG_INFO("{}|{}|{}", message, a, b); });
+    QMPC_LOG_INFO("size: {}", testVec.size());
+    for (const auto &v : testVec)
+    {
+        QMPC_LOG_INFO("v={}", v);
+    }
+    EXPECT_EQ("info", testVec[3]);
+    EXPECT_EQ("info log", testVec[5]);
+    EXPECT_EQ("5", testVec[6]);
+    EXPECT_EQ("3.4", testVec[7]);
 }
 
 TEST(LogTest, DebugTest)
 {
-    std::string message = "debug log";
-    auto testVec = consoleDebug(std::cout, [=]() { qmpc::Log::Debug(message); });
+    qmpc::Log::loadLogLevel("debug");
 
-    EXPECT_EQ("DEBUG", testVec[1]);
-    EXPECT_EQ("debug log\n", testVec[3]);
+    std::string message = "debug log";
+    auto testVec = consoleDebug([=]() { QMPC_LOG_DEBUG(message); });
+
+    qmpc::Log::loadLogLevel(std::nullopt);
+    for (const auto &v : testVec)
+    {
+        QMPC_LOG_INFO("v={}", v);
+    }
+
+    EXPECT_EQ("debug", testVec[3]);
+    EXPECT_EQ("debug log", testVec[5]);
 }
 
 TEST(LogTest, ErrorTest)
 {
     std::string message = "error log";
-    auto testVec = consoleDebug(std::cerr, [message]() { qmpc::Log::Error(message); });
-    EXPECT_EQ("ERROR", testVec[1]);
-    EXPECT_EQ("error log\n", testVec[2]);
+    auto testVec = consoleDebug([message]() { QMPC_LOG_ERROR(message); });
+    EXPECT_EQ("error", testVec[3]);
+    EXPECT_EQ("error log", testVec[5]);
 }
 void f() { qmpc::Log::throw_with_trace(std::runtime_error("runtime error")); }
 TEST(LogTest, exceptionTest)
@@ -60,11 +87,15 @@ TEST(LogTest, exceptionTest)
     catch (const std::exception &e)
     {
         auto testVec = consoleDebug(
-            std::cerr,
-            [&e]() { qmpc::Log::Error(*boost::get_error_info<qmpc::Log::traced>(e), e.what()); }
+            [&e]()
+            { QMPC_LOG_ERROR("{} | {}", *boost::get_error_info<qmpc::Log::traced>(e), e.what()); }
         );
+        for (const auto &v : testVec)
+        {
+            QMPC_LOG_INFO("v={}", v);
+        }
 
-        EXPECT_EQ("ERROR", testVec[1]);
-        EXPECT_EQ("runtime error\n", testVec[3]);
+        EXPECT_EQ("error", testVec[3]);
+        EXPECT_EQ("runtime error", testVec[testVec.size() - 1]);
     }
 }
