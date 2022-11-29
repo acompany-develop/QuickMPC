80a526f0621717c25091976eba94d3c9649e00c2
Shingo Ohtsuka <capriciouswalk@gmail.com>
2022-11-29 17:56:48 +0900

    Define function in wrapper script

--- src/ComputationContainer/Scripts/wrapped_run.sh
+++ src/ComputationContainer/Scripts/wrapped_run.sh
@@ -8,15 +8,65 @@ SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
 CORE_PATTERN_FILE="${SCRIPT_DIR}/data/core_pattern.txt"
 GDB_CMD_FILE="${SCRIPT_DIR}/data/gdb_print_stacktrace_cmd.txt"
 
-set +e
-CORE_PATTERN_CHECK="$(grep "^$(tr -d '\n' < "${CORE_PATTERN_FILE}")$" "${KERNEL_CORE_PATTERN_FILE}")"
-set -e
+function core_pattern_check() {
+  set +e
+  result="$(grep "^$(tr -d '\n' < "${CORE_PATTERN_FILE}")$" "${KERNEL_CORE_PATTERN_FILE}")"
+  set -e
 
-if [ -z "${CORE_PATTERN_CHECK}" ]
-then
-  echo "WARNING: ${KERNEL_CORE_PATTERN_FILE} does not match what QuickMPC expects."
-  echo "WARNING: Please run ${SCRIPT_DIR}/host_core_setting.sh on host machine."
-fi
+  if [ -z "${result}" ]
+  then
+    echo "WARNING: ${KERNEL_CORE_PATTERN_FILE} does not match what QuickMPC expects."
+    echo "WARNING: Please run ${SCRIPT_DIR}/host_core_setting.sh on host machine."
+
+    return 1
+  fi
+
+  return 0
+}
+
+function run()
+{
+  set +e
+  env "${@:1:$(($#-1))}" "${@: -1}"
+
+  status=$?
+  set -e
+
+  if [ $status -eq 0 ]
+  then
+    return 0
+  fi
+
+  echo "INFO: ${*: -1} exit with code: ${status}"
+
+  if ! core_pattern_check
+  then
+    return 1
+  fi
+
+  # find executable file
+  executable_file=""
+
+  for arg in "$@"
+  do
+    if [[ -x "$arg" ]]
+    then
+      executable_file="$arg"
+    fi
+  done
+
+  # find latest core file
+  core_file=$(find /tmp/cores/ -maxdepth 1 -type f -exec stat -c '%Y %n' {} \; | sort -nr | awk 'NR==1,NR==3 {print $2}' | head -1)
+
+  if [[ "$core_file" == "" ]]
+  then
+    echo 'WARNING: core file was not found'
+    return 1
+  fi
+
+  gdb "${executable_file}" "${core_file}" -x "${GDB_CMD_FILE}" -batch -quiet
+}
+
 
 if [ $# -le 0 ]
 then
@@ -25,43 +75,9 @@ then
   exit 1
 fi
 
-# run
-set +e
-env "${@:1:$(($#-1))}" "${@: -1}"
+core_pattern_check
 
-STATUS=$?
-set -e
-
-if [ $STATUS -eq 0 ]
-then
-  exit 0
-fi
-
-# core pattern check
-if [ -z "${CORE_PATTERN_CHECK}" ]
-then
-  echo "WARNING: ${KERNEL_CORE_PATTERN_FILE} does not match what QuickMPC expects."
-  echo "WARNING: core file analyzing is skipped."
-  exit $STATUS
-fi
-
-# find executable file
-executable_file=""
-
-for arg in "$@"
+while true
 do
-  if [[ -x "$arg" ]]
-  then
-    executable_file="$arg"
-  fi
+  run
 done
-
-# find latest core file
-core_file=$(find /tmp/cores/ -maxdepth 1 -type f -exec stat -c '%Y %n' {} \; | sort -nr | awk 'NR==1,NR==3 {print $2}' | head -1)
-
-if [ $STATUS -ne 0 ]
-then
-  gdb "${executable_file}" "${core_file}" -x "${GDB_CMD_FILE}" -batch -quiet
-fi
-
-exit $STATUS
