bf7641008d13b626106fc0d89ba47dd5474574ca
Shingo Ohtsuka <capriciouswalk@gmail.com>
2022-12-13 07:25:07 +0000

    Use multi-thread at CC2CC comm.

--- src/ComputationContainer/Client/ComputationToComputationContainer/Client.hpp
+++ src/ComputationContainer/Client/ComputationToComputationContainer/Client.hpp
@@ -6,7 +6,11 @@
 #include <grpcpp/create_channel.h>
 #include <grpcpp/security/credentials.h>
 
+#include <exception>
+#include <future>
 #include <iostream>
+#include <iterator>
+#include <memory>
 #include <string>
 
 #include "Client/Helper/Helper.hpp"
@@ -21,7 +25,7 @@ class Client
     friend class Server;
 
 private:
-    std::unique_ptr<computationtocomputation::ComputationToComputation::Stub> stub_;
+    std::vector<std::unique_ptr<computationtocomputation::ComputationToComputation::Stub>> stubs_;
     Client() noexcept = default;
     ~Client() noexcept = default;
     Client(const Url &endpoint) noexcept;
@@ -66,13 +70,42 @@ private:
         return s;
     }
 
+    template <typename T>
+    using iterator_type = typename std::vector<T>::const_iterator;
+    template <typename T>
+    using range_type = std::pair<iterator_type<T>, iterator_type<T>>;
+
+    template <typename T>
+    std::vector<range_type<T>> split(const std::vector<T> &input) const
+    {
+        std::size_t threads = this->stubs_.size();
+        if (threads == 0)
+        {
+            throw std::invalid_argument("");
+        }
+        std::vector<range_type<T>> ret;
+
+        const std::size_t width = (input.size() + threads - 1) / threads;
+        for (std::size_t i = 0; i < threads; ++i)
+        {
+            const std::size_t step_from = std::min(input.size(), i * width);
+            const iterator_type<T> from = std::next(input.cbegin(), step_from);
+            const std::size_t step_to = std::min(input.size(), (i + 1) * width);
+            const iterator_type<T> to = std::next(input.cbegin(), step_to);
+
+            ret.emplace_back(std::make_pair(from, to));
+        }
+
+        return ret;
+    }
+
     // 複数シェアを生成する場合
     // 約1mbごとに分割して生成する
     template <typename T>
     std::vector<computationtocomputation::Shares> makeShares(
-        const std::vector<T> &values,
-        const std::vector<qmpc::Share::AddressId> &share_ids,
-        unsigned int length,
+        typename std::vector<T>::const_iterator values,
+        typename std::vector<qmpc::Share::AddressId>::const_iterator &share_ids,
+        std::size_t length,
         int party_id
     ) const
     {
@@ -81,10 +114,10 @@ private:
         size_t addressId_size = sizeof(qmpc::Share::AddressId);
         size_t size = 0;
         computationtocomputation::Shares s;
-        for (unsigned int i = 0; i < length; i++)
+        for (std::size_t i = 0; i < length; i++)
         {
             // string型のバイト数の取得
-            size_t value_size = sizeof(values[i]);
+            size_t value_size = sizeof(*values);
             // ShareId,JobId,ThreadId,PartyIdの16byte
             if (size + value_size + addressId_size + sizeof(s.party_id()) > 1000000)
             {
@@ -97,32 +130,34 @@ private:
             size = size + value_size + addressId_size;
             computationtocomputation::Shares_Share *multiple_shares = s.add_share_list();
             auto a = multiple_shares->mutable_address_id();
-            a->set_share_id(share_ids[i].getShareId());
-            a->set_job_id(share_ids[i].getJobId());
+            a->set_share_id(share_ids->getShareId());
+            a->set_job_id(share_ids->getJobId());
             if constexpr (std::is_same_v<std::decay_t<T>, bool>)
             {
-                multiple_shares->set_flag(values[i]);
+                multiple_shares->set_flag(*values);
             }
             else if constexpr (std::is_same_v<std::decay_t<T>, int>)
             {
-                multiple_shares->set_num(values[i]);
+                multiple_shares->set_num(*values);
             }
             else if constexpr (std::is_same_v<std::decay_t<T>, long>)
             {
-                multiple_shares->set_num64(values[i]);
+                multiple_shares->set_num64(*values);
             }
             else if constexpr (std::is_same_v<std::decay_t<T>, float>)
             {
-                multiple_shares->set_f(values[i]);
+                multiple_shares->set_f(*values);
             }
             else if constexpr (std::is_same_v<std::decay_t<T>, double>)
             {
-                multiple_shares->set_d(values[i]);
+                multiple_shares->set_d(*values);
             }
             else
             {
-                multiple_shares->set_byte(to_string(values[i]));
+                multiple_shares->set_byte(to_string(*values));
             }
+            values++;
+            share_ids++;
         }
         s.set_party_id(party_id);
         share_vec.push_back(s);
@@ -147,7 +182,7 @@ public:
         do
         {
             grpc::ClientContext context;
-            status = stub_->ExchangeShare(&context, share, &response);
+            status = stubs_[0]->ExchangeShare(&context, share, &response);
         } while (retry_manager.retry(status));
 
         // 送信に成功
@@ -163,31 +198,71 @@ public:
         int party_id
     ) const
     {
-        // リクエスト設定
-        std::vector<computationtocomputation::Shares> shares;
-        google::protobuf::Empty response;
-        shares = makeShares(values, share_ids, length, party_id);
-        grpc::Status status;
+        const std::vector<range_type<T>> values_ranges = split(values);
+        const std::vector<range_type<qmpc::Share::AddressId>> share_ids_ranges = split(share_ids);
 
-        // リトライポリシーに従ってリクエストを送る
-        auto retry_manager = RetryManager("CC", "exchangeShares");
-        do
+        std::vector<std::future<std::exception_ptr>> threads;
+
+        for (std::size_t i = 0; i < stubs_.size(); ++i)
         {
-            grpc::ClientContext context;
-            std::shared_ptr<grpc::ClientWriter<computationtocomputation::Shares>> stream(
-                stub_->ExchangeShares(&context, &response)
-            );
-            for (size_t i = 0; i < shares.size(); i++)
+            if (std::distance(values_ranges[i].first, values_ranges[i].second) == 0)
             {
-                if (!stream->Write(shares[i]))
-                {
-                    // Broken stream.
-                    break;
-                }
+                continue;
             }
-            stream->WritesDone();
-            status = stream->Finish();
-        } while (retry_manager.retry(status));
+            threads.emplace_back(std::async(
+                std::launch::async,
+                [i, values_ranges, share_ids_ranges, party_id, &stubs_ = this->stubs_, this](
+                ) -> std::exception_ptr
+                {
+                    try
+                    {
+                        iterator_type<T> from = values_ranges[i].first;
+                        iterator_type<T> to = values_ranges[i].second;
+                        iterator_type<qmpc::Share::AddressId> s_from = share_ids_ranges[i].first;
+                        const std::size_t len = std::distance(from, to);
+                        // リクエスト設定
+                        std::vector<computationtocomputation::Shares> shares;
+                        google::protobuf::Empty response;
+                        shares = makeShares<T>(from, s_from, len, party_id);
+                        grpc::Status status;
+
+                        // リトライポリシーに従ってリクエストを送る
+                        auto retry_manager = RetryManager("CC", "exchangeShares");
+                        do
+                        {
+                            grpc::ClientContext context;
+                            std::shared_ptr<grpc::ClientWriter<computationtocomputation::Shares>>
+                                stream(stubs_[i]->ExchangeShares(&context, &response));
+                            for (size_t i = 0; i < shares.size(); i++)
+                            {
+                                if (!stream->Write(shares[i]))
+                                {
+                                    // Broken stream.
+                                    break;
+                                }
+                            }
+                            stream->WritesDone();
+                            status = stream->Finish();
+                        } while (retry_manager.retry(status));
+                    }
+                    catch (...)
+                    {
+                        return std::current_exception();
+                    }
+
+                    return nullptr;
+                }
+            ));
+        }
+
+        for (std::future<std::exception_ptr> &future : threads)
+        {
+            std::exception_ptr ep = future.get();
+            if (ep != nullptr)
+            {
+                std::rethrow_exception(ep);
+            }
+        }
 
         // 送信に成功
         return true;
