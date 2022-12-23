eb4f7a5d90a915f1bfdd23cc654a7241e5b72e76
Nakata <ryosuke.nakata@acompany-ac.com>
2022-12-23 16:54:22 +0900

    Add timeout to sync wait

--- src/ManageContainer/Client/ManageToManageContainer/Client.go
+++ src/ManageContainer/Client/ManageToManageContainer/Client.go
@@ -110,9 +110,19 @@ func (c Client) Sync(syncID string) error {
 		}
 	}
 
-	// 他のMCからリクエストが来るのを待機
-	m2mserver.Wait(syncID, func(cnt int) bool { return cnt < len(connList) })
-	return nil
+	// 他のMCからリクエストが来るのをタイムアウト付きで待機
+	timeout := time.After(50 * time.Second)
+	done := make(chan struct{})
+	go func() {
+		m2mserver.Wait(syncID, func(cnt int) bool { return cnt < len(connList) })
+		done <- struct{}{}
+	}()
+	select {
+	case <-timeout:
+		return fmt.Errorf("Sync response is not returned ERROR!")
+	case <-done:
+		return nil
+	}
 }
 
 // (conn)にシェア削除リクエストを送信する
