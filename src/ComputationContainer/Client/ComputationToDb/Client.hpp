a43b6f7a8463588efee5bd32d0da7b8b8ec9d941
root <root@ktakahashi-dev.us-central1-a.c.noted-gizmo-337508.internal>
2023-01-04 05:12:59 +0000

    Fix writeComputationResult

--- src/ComputationContainer/Client/ComputationToDb/Client.hpp
+++ src/ComputationContainer/Client/ComputationToDb/Client.hpp
@@ -46,26 +46,76 @@ public:
     // resultの保存
     template <class T>
     void writeComputationResult(
-        const std::string &job_uuid, const T &results, int piece_size = 1000000
+        const std::string &job_uuid, const T &results, bool is_schema=false, int piece_size = 1000000
     ) const
     {
-        const nlohmann::json data_json = results;
-        const std::string data_str = data_json.dump();
+        // resultsの形式は1次元 or 2次元 or json
+
+        // NOTE: 現バージョンでresultsがjsonとなるのは結合リクエストのみ
+        // 結合リクエストの場合はschemaとtableそれぞれで保存処理
+        if constexpr(std::is_same_v<std::decay_t<T>, nlohmann::json>){
+            std::vector<std::string> schema = results["schema"];
+            std::vector<std::vector<std::string>> table = results["table"];
+            writeComputationResult(job_uuid,schema,true);
+            writeComputationResult(job_uuid,table);
+            return;
+        }
+
+        int column_number = -1;
+        std::vector<std::string> result_list;
+        if constexpr(std::is_same_v<std::decay_t<T>, std::vector<std::string>>)
+        {
+            column_number = results.size();
+            result_list.reserve(results.size());
+            result_list = results;
+        }
+        else if constexpr(std::is_same_v<std::decay_t<T>, std::vector<std::vector<std::string>>>)
+        {
+            // ２次元配列の場合は1次元に変換する
+            column_number = results[0].size();
+            size_t size = results.size() * results[0].size();
+            result_list.reserve(size);
+
+            for (auto row : results){
+                for (auto val : row){
+                    result_list.push_back(val);
+                }
+            }
+        }
+        else
+        {
+            // TODO: throw
+        }
 
         // 巨大なデータはpieceに分割して保存する
-        for (size_t left = 0; left < data_str.size(); left += piece_size)
-        {
-            const auto piece_str = data_str.substr(left, piece_size);
-            const int piece_id = left / piece_size;
+        int piece_id = 0;
+        int tmp = 0;
+        std::vector<std::string> piece_result;
+        for (auto val : result_list){
+            if (tmp+val.size()>=piece_size){
+                nlohmann::json piece_data_json = {
+                    {"job_uuid", job_uuid}, {"result", piece_result}, {"meta", {{"piece_id", piece_id},{"column_number", column_number}}}};
+                const std::string data = piece_data_json.dump();
 
-            nlohmann::json piece_data_json = {
-                {"job_uuid", job_uuid}, {"result", piece_str}, {"meta", {{"piece_id", piece_id}}}};
-            const std::string data = piece_data_json.dump();
+                auto ofs = std::ofstream(resultDbPath + job_uuid + "/" + (is_schema? "schema":"") + std::to_string(piece_id));
+                ofs << data;
+                ofs.close();
 
-            auto ofs = std::ofstream(resultDbPath + job_uuid + "/" + std::to_string(piece_id));
-            ofs << data;
-            ofs.close();
+                tmp = 0;
+                piece_id++;
+                piece_result.clear();
+            }
+            piece_result.push_back(val);
+            tmp += val.size();
         }
+        nlohmann::json piece_data_json = {
+            {"job_uuid", job_uuid}, {"result", piece_result}, {"meta", {{"piece_id", piece_id},{"column_number", column_number}}}};
+        const std::string data = piece_data_json.dump();
+
+        auto ofs = std::ofstream(resultDbPath + job_uuid + "/" + (is_schema? "schema":"") + std::to_string(piece_id));
+        ofs << data;
+        ofs.close();
+
         std::ofstream(resultDbPath + job_uuid + "/completed");
     }
 };
