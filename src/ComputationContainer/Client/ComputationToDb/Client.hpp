1782d5dfcdc6702bca70c88e340bad533d66f467
root <kota.takahashi@acompany-ac.com>
2023-01-04 07:39:48 +0000

    Update computation result type

--- src/ComputationContainer/Client/ComputationToDb/Client.hpp
+++ src/ComputationContainer/Client/ComputationToDb/Client.hpp
@@ -46,38 +46,47 @@ public:
     // resultの保存
     template <class T>
     void writeComputationResult(
-        const std::string &job_uuid, const T &results, bool is_schema=false, int piece_size = 1000000
+        const std::string &job_uuid,
+        const T &results,
+        bool is_schema = false,
+        int piece_size = 1000000
     ) const
     {
         // resultsの形式は1次元 or 2次元 or json
 
         // NOTE: 現バージョンでresultsがjsonとなるのは結合リクエストのみ
         // 結合リクエストの場合はschemaとtableそれぞれで保存処理
-        if constexpr(std::is_same_v<std::decay_t<T>, nlohmann::json>){
+        if constexpr (std::is_same_v<std::decay_t<T>, nlohmann::json>)
+        {
             std::vector<std::string> schema = results["schema"];
             std::vector<std::vector<std::string>> table = results["table"];
-            writeComputationResult(job_uuid,schema,true);
-            writeComputationResult(job_uuid,table);
+            writeComputationResult(job_uuid, schema, true);
+            writeComputationResult(job_uuid, table);
             return;
         }
 
+        std::string data_name = "";
         int column_number = -1;
         std::vector<std::string> result_list;
-        if constexpr(std::is_same_v<std::decay_t<T>, std::vector<std::string>>)
+        if constexpr (std::is_same_v<std::decay_t<T>, std::vector<std::string>>)
         {
+            data_name = "dim1";
             column_number = results.size();
             result_list.reserve(results.size());
             result_list = results;
         }
-        else if constexpr(std::is_same_v<std::decay_t<T>, std::vector<std::vector<std::string>>>)
+        else if constexpr (std::is_same_v<std::decay_t<T>, std::vector<std::vector<std::string>>>)
         {
             // ２次元配列の場合は1次元に変換する
+            data_name = "dim2";
             column_number = results[0].size();
             size_t size = results.size() * results[0].size();
             result_list.reserve(size);
 
-            for (auto row : results){
-                for (auto val : row){
+            for (auto row : results)
+            {
+                for (auto val : row)
+                {
                     result_list.push_back(val);
                 }
             }
@@ -86,18 +95,28 @@ public:
         {
             // TODO: throw
         }
+        if (is_schema)
+        {
+            data_name = "schema";
+        }
 
         // 巨大なデータはpieceに分割して保存する
         int piece_id = 0;
         int tmp = 0;
         std::vector<std::string> piece_result;
-        for (auto val : result_list){
-            if (tmp+val.size()>=piece_size){
+        for (auto val : result_list)
+        {
+            if (tmp + static_cast<int>(val.size()) >= piece_size)
+            {
                 nlohmann::json piece_data_json = {
-                    {"job_uuid", job_uuid}, {"result", piece_result}, {"meta", {{"piece_id", piece_id},{"column_number", column_number}}}};
+                    {"job_uuid", job_uuid},
+                    {"result", piece_result},
+                    {"meta", {{"piece_id", piece_id}, {"column_number", column_number}}}};
                 const std::string data = piece_data_json.dump();
 
-                auto ofs = std::ofstream(resultDbPath + job_uuid + "/" + (is_schema? "schema":"") + std::to_string(piece_id));
+                auto ofs = std::ofstream(
+                    resultDbPath + job_uuid + "/" + data_name + "_" + std::to_string(piece_id)
+                );
                 ofs << data;
                 ofs.close();
 
@@ -109,10 +128,14 @@ public:
             tmp += val.size();
         }
         nlohmann::json piece_data_json = {
-            {"job_uuid", job_uuid}, {"result", piece_result}, {"meta", {{"piece_id", piece_id},{"column_number", column_number}}}};
+            {"job_uuid", job_uuid},
+            {"result", piece_result},
+            {"meta", {{"piece_id", piece_id}, {"column_number", column_number}}}};
         const std::string data = piece_data_json.dump();
 
-        auto ofs = std::ofstream(resultDbPath + job_uuid + "/" + (is_schema? "schema":"") + std::to_string(piece_id));
+        auto ofs = std::ofstream(
+            resultDbPath + job_uuid + "/" + data_name + "_" + std::to_string(piece_id)
+        );
         ofs << data;
         ofs.close();
 
