6b3c607ff61ed311030806a7b4ece8479dbdc91c
root <kota.takahashi@acompany-ac.com>
2023-01-11 04:17:55 +0000

    Fix get_result

--- Test/LibClient/src/utils.py
+++ Test/LibClient/src/utils.py
@@ -9,6 +9,28 @@ qmpc: QMPC = QMPC([
     "http://localhost:9003",
 ])
 
+def __try_get_computation_result(job_uuid, is_limit):
+    try:
+        get_res = qmpc.get_computation_result(job_uuid)
+    except:
+        if is_limit:
+            raise
+        return None
+
+
+    all_completed = False
+    if get_res["statuses"] is not None:
+        all_completed = all([status == JobStatus.COMPLETED
+                                for status in get_res["statuses"]])
+
+    if all_completed:
+        # NOTE
+        # 計算結果取得時には含まれていないが，job_uuidがmodelの取得に使われるので
+        # このような処理をしている
+        get_res["job_uuid"] = job_uuid
+        return get_res
+
+    return None
 
 def get_result(res, limit=20):
     """ 計算が終わるまで結果取得をし続ける(5秒x20回) """
@@ -17,19 +39,8 @@ def get_result(res, limit=20):
 
     job_uuid = res["job_uuid"]
 
-    for _ in range(limit):
-        get_res = qmpc.get_computation_result(job_uuid)
-
-        all_completed = False
-        if get_res["statuses"] is not None:
-            all_completed = all([status == JobStatus.COMPLETED
-                                 for status in get_res["statuses"]])
-
-        if all_completed:
-            # NOTE
-            # 計算結果取得時には含まれていないが，job_uuidがmodelの取得に使われるので
-            # このような処理をしている
-            get_res["job_uuid"] = job_uuid
-            return get_res
-
+    for i in range(limit):
+        res = __try_get_computation_result(job_uuid, i+1 == limit)
+        if res is not None:
+            return res
         time.sleep(5)
