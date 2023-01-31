a482d3f1663e17d6feba20362885970530012bc0
Nakata <ryosuke.nakata@acompany-ac.com>
2023-01-31 13:42:12 +0900

    Fix token test

--- scripts/LibClient/src/tests/test_token.py
+++ scripts/LibClient/src/tests/test_token.py
@@ -10,12 +10,7 @@ from utils import get_result, qmpc
 filename_table: str = "Data/table_data_5x5.csv"
 secrets, schema = qmpc.parse_csv_file(filename_table)
 length: int = len(secrets[0])
-filename_model: str = "Data/model_data_a6.csv"
 inp = [i for i in range(2, length + 1)]
-data = []
-with open(filename_model) as f:
-    reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-    data = [row for row in reader][0]
 
 
 def __preprocess():
@@ -28,10 +23,7 @@ def __preprocess():
     get_result(res_execute_computation)
     job_uuid: str = res_execute_computation["job_uuid"]
 
-    res_send_model_params = qmpc.send_model_params(data)
-    model_param_job_uuid: str = res_send_model_params["job_uuid"]
-
-    return data_id, job_uuid, model_param_job_uuid
+    return data_id, job_uuid
 
 
 def check_all_request(token: str):
@@ -41,28 +33,25 @@ def check_all_request(token: str):
          "http://localhost:9003"],
         token
     )
-    data_id, job_uuid, model_param_job_uuid = __preprocess()
+    data_id, job_uuid = __preprocess()
     table = [[data_id], [], [1]]
 
     # 各requestを実行する
     requests = [
-        (qmpc_inner.send_share,(secrets, schema)),
-        (qmpc_inner.sum,(table, inp)),
-        (qmpc_inner.get_computation_result,(job_uuid,)),
-        (qmpc_inner.send_model_params,(data,)),
-        (qmpc_inner.linear_regression_predict,
-         (model_param_job_uuid, table, inp)),
-        (qmpc_inner.delete_share,([data_id],))
+        (qmpc_inner.send_share, (secrets, schema)),
+        (qmpc_inner.sum, (table, inp)),
+        (qmpc_inner.get_computation_result, (job_uuid,)),
+        (qmpc_inner.delete_share, ([data_id],))
     ]
 
-    def try_func(func,param):
+    def try_func(func, param):
         try:
             return func(*param)["is_ok"]
         except QMPCServerError:
             """tokenが正しくない場合例外が投げられるため"""
             return False
 
-    response = [try_func(func,param) for func,param in requests]
+    response = [try_func(func, param) for func, param in requests]
 
     return response
 
