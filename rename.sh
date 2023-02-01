20dba8917859a1742fe4ff7a93d7aae89593e753
come2ry <tkrk.audio@gmail.com>
2023-02-01 05:57:19 +0000

    Rename _lower_snake_case -> lower_snake_case

--- rename.sh
+++ rename.sh
@@ -1,6 +1,6 @@
 find . -depth -type d -name '_*[a-z]*' ! -path "./.git/*" ! -path "./.github/*" | while read dir; do
   base=$(basename "$dir")
   # new_dir=$(echo "$base" | sed -r 's/([A-Z][a-z0-9]+)/_\1/g' | tr '[:upper:]' '[:lower:]')
-  echo mv "$dir" "$(dirname "$dir")/${base#*_}"
-  # mv "$dir" "$(dirname "$dir")/${new_dir}"
+  # echo mv "$dir" "$(dirname "$dir")/${base#*_}"
+  mv "$dir" "$(dirname "$dir")/${base#*_}"
 done
