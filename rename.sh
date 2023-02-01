46552395dd99381694a1e7292842a00dcc9cb173
come2ry <tkrk.audio@gmail.com>
2023-02-01 05:53:57 +0000

    Rename UpperCapitalCase -> _lower_snake_case

--- rename.sh
+++ rename.sh
@@ -1,6 +1,6 @@
-find . -depth -type d -name '*[A-Z]*' ! -path ""| while read dir; do
+find . -depth -type d -name '*[A-Z]*' ! -path "./.git/*" ! -path "./.github/*" | while read dir; do
   base=$(basename "$dir")
   new_dir=$(echo "$base" | sed -r 's/([A-Z][a-z0-9]+)/_\1/g' | tr '[:upper:]' '[:lower:]')
-  echo mv "$dir" "$(dirname "$dir")/${new_dir#*_}"
-#   mv "$dir" "$(dirname "$dir")/${new_dir#*_}"
+#   echo mv "$dir" "$(dirname "$dir")/${new_dir}"
+  mv "$dir" "$(dirname "$dir")/${new_dir}"
 done
