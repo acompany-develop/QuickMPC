e1a0f813271de8345724200f6b5e8726c98fbae4
come2ry <tkrk.audio@gmail.com>
2023-02-01 05:34:53 +0000

    Add rename.sh

---
+++ rename.sh
@@ -0,0 +1,6 @@
+find . -depth -type d -name '*[A-Z]*' ! -path ""| while read dir; do
+  base=$(basename "$dir")
+  new_dir=$(echo "$base" | sed -r 's/([A-Z][a-z0-9]+)/_\1/g' | tr '[:upper:]' '[:lower:]')
+  echo mv "$dir" "$(dirname "$dir")/${new_dir#*_}"
+#   mv "$dir" "$(dirname "$dir")/${new_dir#*_}"
+done
