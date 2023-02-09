d565e855f7043a8eee26626a19edaa7541be26bb
root <kota.takahashi@acompany-ac.com>
2023-02-09 10:38:34 +0000

    Add member variables

--- packages/server/beaver_triple_service/jwt/types.go
+++ packages/server/beaver_triple_service/jwt/types.go
@@ -3,12 +3,15 @@ package jwt
 import "github.com/golang-jwt/jwt/v4"
 
 type Party struct {
-	Id      int    `json:"id"`
+	Id      uint32    `json:"id"`
 	Address string `json:"address"`
 }
 
 type Claim struct {
 	RoomUUID  string  `json:"room_uuid"`
+	Port      uint32  `json:"port"`
+	PartyNum  uint32  `json:"party_num"`
 	PartyInfo []Party `json:"party_info"`
+	WithEnvoy bool    `json:"with_envoy"`
 	jwt.RegisteredClaims
 }
