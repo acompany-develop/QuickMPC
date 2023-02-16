66b749f17e38393d885b27ef031e13bcaa2cd389
root <kota.takahashi@acompany-ac.com>
2023-02-16 05:54:40 +0000

    Add intercepter.go

---
+++ packages/server/beaver_triple_service/utils/interceptor.go
@@ -0,0 +1,104 @@
+package utils
+
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"os"
+
+	"github.com/golang-jwt/jwt/v4"
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	jwt_types "github.com/acompany-develop/QuickMPC/packages/server/beaver_triple_service/jwt"
+	logger "github.com/acompany-develop/QuickMPC/packages/server/beaver_triple_service/log"
+)
+
+func BtsAuthFunc(ctx context.Context) (context.Context, error) {
+	tokenString, err := grpc_auth.AuthFromMD(ctx, "bearer")
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Unauthenticated,
+			"could not read auth token: %v",
+			err,
+		)
+	}
+
+	claims, err := authJWT(tokenString)
+	if err != nil {
+		return nil, err
+	}
+
+	return context.WithValue(ctx, "claims", claims), nil
+}
+
+func getSecret() ([]byte, error) {
+	raw, ok := os.LookupEnv("JWT_SECRET_KEY")
+	if !ok {
+		return nil, status.Error(codes.Internal, "jwt auth key is not provided")
+	}
+	secret, err := base64.StdEncoding.DecodeString(raw)
+	if err != nil {
+		return nil, err
+	}
+	return secret, nil
+}
+
+func authJWT(tokenString string) (*jwt_types.Claim, error) {
+	jwtSecret, err := getSecret()
+	if err != nil {
+		return nil, err
+	}
+
+	token, err := jwt.ParseWithClaims(tokenString, &jwt_types.Claim{}, func(token *jwt.Token) (interface{}, error) {
+		// alg を確認するのを忘れない
+		if signingMethod, ok := token.Method.(*jwt.SigningMethodHMAC); !ok || signingMethod.Alg() != "HS256" {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(jwtSecret), nil
+	})
+
+	if err != nil {
+		return nil, status.Errorf(codes.Unauthenticated, "invalid auth token: %v", err)
+	}
+
+	if !token.Valid {
+		return nil, status.Errorf(codes.Unauthenticated, "invalid auth token: %v", err)
+	}
+
+	claims, ok := token.Claims.(*jwt_types.Claim)
+	if !ok {
+		return nil, status.Error(codes.Internal, "failed claims type assertions")
+	}
+
+	return claims, nil
+}
+
+// requestを受け取った際の共通処理
+func UnaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	// 定期実行されるhealth checkでlogは必要ないため即時return
+	if info.FullMethod == "/grpc.health.v1.Health/Check" {
+		return handler(ctx, req)
+	}
+
+	logger.Infof("received: %s", info.FullMethod)
+
+	// TODO: read claims, and use these party information
+	claims, ok := ctx.Value("claims").(*jwt_types.Claim)
+	if ok {
+		logger.Infof("claims: %v\n", claims)
+	}
+
+	// 処理を実行する
+	res, err := handler(ctx, req)
+
+	// エラー時にログとしてrequest，responseを出力する
+	if err != nil {
+		logger.Errorf("request: {%v}\tresponse: {%v}\n", req, res)
+	}
+
+	logger.Infof("send: %s", info.FullMethod)
+	return res, err
+}
