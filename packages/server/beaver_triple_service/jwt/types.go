48eb348fe6b3828e9a0189830be1c454720abae6
root <kota.takahashi@acompany-ac.com>
2023-02-16 10:50:52 +0000

    Remove unnecessary variables

--- packages/server/beaver_triple_service/jwt/types.go
+++ packages/server/beaver_triple_service/jwt/types.go
@@ -9,9 +9,6 @@ type Party struct {
 
 type Claim struct {
 	RoomUUID  string  `json:"room_uuid"`
-	Port      uint32  `json:"port"`
-	PartyNum  uint32  `json:"party_num"`
 	PartyInfo []Party `json:"party_info"`
-	WithEnvoy bool    `json:"with_envoy"`
 	jwt.RegisteredClaims
 }
