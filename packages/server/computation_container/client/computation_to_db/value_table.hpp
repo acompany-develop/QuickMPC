a9d458b23e6eff1b1a457fe7d271c0ff15075fa1
Nakata <ryosuke.nakata@acompany-ac.com>
2023-02-16 22:50:43 +0900

    Remove Old ValueTable

--- packages/server/computation_container/client/computation_to_db/value_table.hpp
+++ packages/server/computation_container/client/computation_to_db/value_table.hpp
@@ -10,27 +10,6 @@
 
 namespace qmpc::ComputationToDb
 {
-class TableJoiner
-{
-    const std::string data_id;
-
-    std::vector<std::vector<std::string>>
-    getSubTable(const std::optional<std::vector<int>> &, const std::optional<std::vector<int>> &)
-        const;
-    std::vector<std::string> getColumn(int) const;
-    std::string joinDataId(const TableJoiner &, int) const;
-
-public:
-    TableJoiner(const std::string &);
-
-    std::string getDataId() const;
-    std::vector<std::vector<std::string>> getTable() const;
-    std::vector<std::string> getSchemas() const;
-    TableJoiner vjoin(const TableJoiner &, int, int) const;
-    TableJoiner hjoin(const TableJoiner &, int, int) const;
-    TableJoiner hjoinShare(const TableJoiner &, int, int) const;
-};
-
 ValueTable vjoin(const ValueTable &, const ValueTable &, int, int);
 ValueTable hjoin(const ValueTable &, const ValueTable &, int, int);
 ValueTable hjoinShare(const ValueTable &, const ValueTable &, int, int);
