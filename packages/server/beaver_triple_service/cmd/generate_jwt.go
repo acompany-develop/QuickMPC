7bcffacce9870f481d7ead167f24ef5c420f2197
root <kota.takahashi@acompany-ac.com>
2023-02-17 05:03:16 +0000

    Add subcommand

--- packages/server/beaver_triple_service/cmd/generate_jwt.go
+++ packages/server/beaver_triple_service/cmd/generate_jwt.go
@@ -1,4 +1,4 @@
-package main
+package cmd
 
 import (
 	"bufio"
@@ -6,7 +6,6 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -15,9 +14,19 @@ import (
 
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/google/uuid"
+	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
 
 	jwt_types "github.com/acompany-develop/QuickMPC/packages/server/beaver_triple_service/jwt"
+)
+
+type Options struct {
+    file_path  string
+    output_dir string
+}
+
+var (
+    o = &Options{}
 )
 
 func store_client_env(path string, token string) error {
@@ -50,112 +59,117 @@ func store_server_env(path string, secrets string, claim jwt_types.Claim) error 
 	return nil
 }
 
-func main() {
+var generateJwtCmd = &cobra.Command{
+	Use:   "generateJwt",
+	Short: "Generate jwt token",
+	Run: func(cmd *cobra.Command, args []string) {
+		raw, err := os.ReadFile(o.file_path)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// convert yaml to json
+		dict := make(map[string]interface{})
+		err = yaml.Unmarshal(raw, &dict)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		raw, err = json.Marshal(dict)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		var claim jwt_types.Claim
+		err = json.Unmarshal(raw, &claim)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// generate, and set
+		room_uuid, err := uuid.NewRandom()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		claim.RoomUUID = room_uuid.String()
+
+		subject, err := uuid.NewRandom()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		claim.Subject = subject.String()
+
+		// show generated json
+		raw, err = json.Marshal(claim)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		var buf bytes.Buffer
+		err = json.Indent(&buf, raw, "", "  ")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Printf("json: %s\n", buf.String())
+
+		// build token
+		const RANDOM_BITS = 256
+		const RANDOM_BYTES = RANDOM_BITS / 8
+		secrets := make([]byte, RANDOM_BYTES)
+		_, err = rand.Read(secrets)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+		signed_token_str, err := token.SignedString(secrets)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// show token
+		encoded_secrets := base64.StdEncoding.EncodeToString(secrets)
+		log.Printf("token: %s\n", signed_token_str)
+		log.Printf("key base64 encoded: %s\n", encoded_secrets)
+
+		// save part of `.env` files for client and server
+		err = os.MkdirAll(
+			o.output_dir, 0700)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		base := path.Base(o.file_path)
+		ext := path.Ext(base)
+		filename := base[:len(base)-len(ext)]
+
+		err = store_client_env(
+			path.Join(
+				o.output_dir,
+				strings.Join([]string{"client", filename, "env"}, ".")),
+			signed_token_str)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		err = store_server_env(
+			path.Join(
+				o.output_dir,
+				strings.Join([]string{"server", filename, "env"}, ".")),
+			encoded_secrets, claim)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(generateJwtCmd)
 	work_dir, err := os.Getwd()
 	if err != nil {
 		log.Printf("WARN: os.Getwd() is failed, err = %s\n", err)
 	}
-
 	default_inout_root := path.Join(work_dir, "cmd", "jwt_generator", "sample")
-
-	filepath := flag.String("file", path.Join(default_inout_root, "sample.yml"), "configuration file path")
-	output_dir := flag.String("o", default_inout_root, "directory which stores output files")
-	flag.Parse()
-
-	raw, err := os.ReadFile(*filepath)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	// convert yaml to json
-	dict := make(map[string]interface{})
-	err = yaml.Unmarshal(raw, &dict)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	raw, err = json.Marshal(dict)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	var claim jwt_types.Claim
-	err = json.Unmarshal(raw, &claim)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	// generate, and set
-	room_uuid, err := uuid.NewRandom()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	claim.RoomUUID = room_uuid.String()
-
-	subject, err := uuid.NewRandom()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	claim.Subject = subject.String()
-
-	// show generated json
-	raw, err = json.Marshal(claim)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	var buf bytes.Buffer
-	err = json.Indent(&buf, raw, "", "  ")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Printf("json: %s\n", buf.String())
-
-	// build token
-	const RANDOM_BITS = 256
-	const RANDOM_BYTES = RANDOM_BITS / 8
-	secrets := make([]byte, RANDOM_BYTES)
-	_, err = rand.Read(secrets)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
-	signed_token_str, err := token.SignedString(secrets)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	// show token
-	encoded_secrets := base64.StdEncoding.EncodeToString(secrets)
-	log.Printf("token: %s\n", signed_token_str)
-	log.Printf("key base64 encoded: %s\n", encoded_secrets)
-
-	// save part of `.env` files for client and server
-	err = os.MkdirAll(*output_dir, 0700)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	base := path.Base(*filepath)
-	ext := path.Ext(base)
-	filename := base[:len(base)-len(ext)]
-
-	err = store_client_env(
-		path.Join(
-			*output_dir,
-			strings.Join([]string{"client", filename, "env"}, ".")),
-		signed_token_str)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	err = store_server_env(
-		path.Join(
-			*output_dir,
-			strings.Join([]string{"server", filename, "env"}, ".")),
-		encoded_secrets, claim)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	generateJwtCmd.Flags().StringVarP(&o.file_path, "file", "f", path.Join(default_inout_root, "sample.yml"), "configuration file path")
+    generateJwtCmd.Flags().StringVarP(&o.output_dir, "output", "o", default_inout_root, "directory which stores output files")
 }
--- packages/server/beaver_triple_service/cmd/root.go
+++ packages/server/beaver_triple_service/cmd/root.go
@@ -1,9 +1,28 @@
-package main
+package cmd
 
 import (
+	"os"
+
 	e2bserver "github.com/acompany-develop/QuickMPC/packages/server/beaver_triple_service/server/engine_to_bts"
+
+	"github.com/spf13/cobra"
 )
 
-func main() {
-	e2bserver.RunServer()
+var rootCmd = &cobra.Command{
+	Use:   "beaver_triple_service",
+	Short: "Start the beaver_triple_service server",
+	Run: func(cmd *cobra.Command, args []string) {
+		e2bserver.RunServer()
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
