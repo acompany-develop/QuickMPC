f9b7b5dbdae365148030d71621f24c3466229ba4
come2ry <tkrk.audio@gmail.com>
2023-03-22 00:43:37 +0000

    Fix

--- packages/server/beaver_triple_service/jwt/types.go
+++ packages/server/beaver_triple_service/jwt/types.go
@@ -11,8 +11,5 @@ type Claim struct {
 	RoomUUID  string  `json:"room_uuid"`
 	PartyId uint32 `json:"party_id"`
 	PartyInfo []Party `json:"party_info"`
-	Iat  int64  `json:"iat,omitempty"`
-	Nbf int64 `json:"nbf,omitempty"`
-	Exp int64 `json:"exp",omitempty`
 	jwt.RegisteredClaims
 }
