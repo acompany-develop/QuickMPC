ca1512f8573f60d1532ed0c075f7d90158ed2262
root <kota.takahashi@acompany-ac.com>
2023-03-23 06:41:35 +0000

    Update generate_proto.sh

--- proto/generate_proto.sh
+++ proto/generate_proto.sh
@@ -1,19 +1,27 @@
 gen_go(){
-    find $1 -type f -name *proto | xargs -t -I{} bash generate_go.sh {}
+    find $@ -type f -name *proto | xargs -t -I{} bash generate_go.sh {}
 }
 gen_python(){
-    cd ../packages/client/libclient-py/quickmpc/proto
+    cd ../packages/client/libclient-py/
+    pip install .
+    cd quickmpc/proto
     python generate_grpc.py
 }
 
+go_dir=("./*manage*/" "./*bts*/" "./common_types/")
+python_dir=("./*libc*/" "./common_types/")
+
 if [ $# -eq 0 ]; then
-    gen_go "./*manage*"
-    gen_go "./*bts*"
-    gen_go "./common_types"
+    gen_go ${go_dir[@]}
     gen_python
 else
-    gen_go $1
-    if [[ "$1" == *"libc"* || "$1" == *"common_types"* ]]; then
+    if [ "`echo ${go_dir[@]} | grep $1`" ]; then
+        gen_go $1
+    else
+        echo "There is no code that needs to be generated"
+    fi
+
+    if [ "`echo ${python_dir[@]} | grep $1`" ]; then
         gen_python
     fi
 fi
