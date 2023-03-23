e177d2672486606deb292f78665ac3767bf32b9c
root <kota.takahashi@acompany-ac.com>
2023-03-23 07:18:17 +0000

    Fix generate_grpc.py

--- proto/generate_grpc.py
+++ proto/generate_grpc.py
@@ -4,7 +4,7 @@ import autopep8
 import pkg_resources
 from grpc.tools import protoc
 
-_proto_path: str = "./../../../../../proto"
+_output_path: str = "../packages/client/libclient-py/quickmpc/proto"
 
 
 def generate_files():
@@ -13,34 +13,34 @@ def generate_files():
             "",
             "-I{}".format(pkg_resources.resource_filename('grpc_tools',
                                                           '_proto')),
-            f"-I{_proto_path}/libc_to_manage_container",
-            f"-I{_proto_path}",
-            "--python_out=.",
-            "--grpc_python_out=.",
-            "--mypy_out=.",
-            "--mypy_grpc_out=.",
-            f"{_proto_path}/libc_to_manage_container/libc_to_manage.proto",
+            "-I./libc_to_manage_container",
+            "-I./",
+            f"--python_out={_output_path}",
+            f"--grpc_python_out={_output_path}",
+            f"--mypy_out={_output_path}",
+            f"--mypy_grpc_out={_output_path}",
+            "./libc_to_manage_container/libc_to_manage.proto",
         )
     )
     protoc.main(
         (
             "",
-            f"-I{_proto_path}",
-            "--python_out=.",
-            "--mypy_out=.",
-            f"{_proto_path}/common_types/common_types.proto",
+            "-I./",
+            f"--python_out={_output_path}",
+            f"--mypy_out={_output_path}",
+            "./common_types/common_types.proto",
         )
     )
 
 
 def format_files():
-    files = glob.glob("./**/*.py", recursive=True)
+    files = glob.glob(f"{_output_path}/**/*.py", recursive=True)
     for file in files:
         autopep8.main(["autopep8", "--in-place", file])
 
 
 if __name__ == '__main__':
-    print(f"path: `{_proto_path}`")
+    print(f"path: `./`")
     generate_files()
     print("generate finished")
     format_files()
