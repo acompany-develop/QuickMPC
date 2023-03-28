9edcba1545e8a3b53621aea255d51fc556786769
Nakata <ryosuke.nakata@acompany-ac.com>
2023-03-28 22:02:57 +0900

    Clean container_test

--- scripts/container_test/tests/test_down_request.py
+++ scripts/container_test/tests/test_down_request.py
@@ -1,50 +1,17 @@
 import time
 
 import pytest
-from quickmpc import JobStatus
 
-from utils import all_containers, bts_p, cc_p, mc_all, mc_p, qmpc
-
-
-def __send_share() -> dict:
-    return qmpc.send_share([[1, 2, 3], [4, 5, 6]], ["a", "b", "c"])
-
-
-def __data_id() -> str:
-    res_ss = __send_share()
-    assert res_ss["is_ok"]
-    return res_ss["data_id"]
-
-
-def __execute_computation(data_id1: str, data_id2: str) -> dict:
-    return qmpc.get_join_table(([data_id1, data_id2], [2], [1, 1]))
-
-
-def __job_uuid() -> str:
-    res_ec = __execute_computation(__data_id(), __data_id())
-    assert res_ec["is_ok"]
-    job_uuid: str = res_ec["job_uuid"]
-    # 計算結果が保存されるまで待機する
-    for _ in range(10):
-        try:
-            res = qmpc.get_computation_result(job_uuid)
-        except Exception:
-            continue
-        if res["statuses"] is not None:
-            if all([status == JobStatus.COMPLETED
-                    for status in res["statuses"]]):
-                return job_uuid
-        time.sleep(3)
-    raise RuntimeError("`execute` is not completed.")
-
-
-def __get_computation_result(job_uuid: str) -> dict:
-    return qmpc.get_computation_result(job_uuid)
+from utils import (all_containers, bts_p, cc_p, data_id, execute_computation,
+                   get_computation_result, job_uuid, mc_all, mc_p, qmpc,
+                   send_share)
 
 
 @pytest.mark.parametrize(
     ("down_container"), [
-        (mc_p(1))
+        (mc_p(1)),
+        (mc_p(2)),
+        (mc_p(3)),
     ]
 )
 def test_failed_send_share_with_down(down_container):
@@ -55,7 +22,7 @@ def test_failed_send_share_with_down(down_container):
 
     # コンテナをdownさせてからsend_shareを送る
     down_container.down()
-    res = __send_share()
+    res = send_share()
     mc.down()
 
     assert not res["is_ok"]
@@ -63,7 +30,13 @@ def test_failed_send_share_with_down(down_container):
 
 @pytest.mark.parametrize(
     ("down_container"), [
-        (cc_p(1))
+        (mc_p(1)),
+        (mc_p(2)),
+        (mc_p(3)),
+        (cc_p(1)),
+        (cc_p(2)),
+        (cc_p(3)),
+        (bts_p()),
     ]
 )
 def test_failed_execute_computations_with_down(down_container):
@@ -73,12 +46,12 @@ def test_failed_execute_computations_with_down(down_container):
     ac.up()
 
     # コンテナを落とす前にsend_shareしておく
-    data_id1: str = __data_id()
-    data_id2: str = __data_id()
+    data_id1: str = data_id()
+    data_id2: str = data_id()
 
     # コンテナをdownさせてからexecute_computation(hjoin)を送る
     down_container.down()
-    res = __execute_computation(data_id1, data_id2)
+    res = execute_computation(data_id1, data_id2)
     ac.down()
 
     assert not res["is_ok"]
@@ -86,7 +59,9 @@ def test_failed_execute_computations_with_down(down_container):
 
 @pytest.mark.parametrize(
     ("down_container"), [
-        (mc_p(1))
+        (mc_p(1)),
+        (mc_p(2)),
+        (mc_p(3)),
     ]
 )
 def test_failed_get_computation_result_with_down(down_container):
@@ -96,11 +71,11 @@ def test_failed_get_computation_result_with_down(down_container):
     ac.up()
 
     # コンテナを落とす前にexecuteしておく
-    job_uuid: str = __job_uuid()
+    job_uuid1: str = job_uuid()
 
     # コンテナをdownさせてからget_compuation_resultを送る
     down_container.down()
-    res = __get_computation_result(job_uuid)
+    res = get_computation_result(job_uuid1)
     ac.down()
 
     assert not res["is_ok"]
--- scripts/container_test/tests/utils.py
+++ scripts/container_test/tests/utils.py
@@ -1,13 +1,10 @@
-from quickmpc import QMPC
+import time
+
+from quickmpc import QMPC, JobStatus
 
 from container import Containers
 
-qmpc: QMPC = QMPC([
-    "http://localhost:50001",
-    "http://localhost:50002",
-    "http://localhost:50003",
-])
-
+""" 各種コンテナ """
 __cc_names = [
     "dev_unit_cc1",
     "dev_unit_cc_envoy1",
@@ -53,3 +50,47 @@ def bts_p() -> Containers:
 
 def all_containers() -> Containers:
     return Containers(__cc_names+__mc_names+__bts_names)
+
+
+""" quickmpcへ簡易request群 """
+qmpc: QMPC = QMPC([
+    "http://localhost:50001",
+    "http://localhost:50002",
+    "http://localhost:50003",
+])
+
+
+def send_share() -> dict:
+    return qmpc.send_share([[1, 2, 3], [4, 5, 6]], ["a", "b", "c"])
+
+
+def data_id() -> str:
+    res_ss = send_share()
+    assert res_ss["is_ok"]
+    return res_ss["data_id"]
+
+
+def execute_computation(data_id1: str, data_id2: str) -> dict:
+    return qmpc.get_join_table(([data_id1, data_id2], [2], [1, 1]))
+
+
+def job_uuid() -> str:
+    res_ec = execute_computation(data_id(), data_id())
+    assert res_ec["is_ok"]
+    job_uuid_: str = res_ec["job_uuid"]
+    # 計算結果が保存されるまで待機する
+    for _ in range(10):
+        try:
+            res = qmpc.get_computation_result(job_uuid)
+        except Exception:
+            continue
+        if res["statuses"] is not None:
+            if all([status == JobStatus.COMPLETED
+                    for status in res["statuses"]]):
+                return job_uuid_
+        time.sleep(3)
+    raise RuntimeError("`execute` is not completed.")
+
+
+def get_computation_result(job_uuid: str) -> dict:
+    return qmpc.get_computation_result(job_uuid)
