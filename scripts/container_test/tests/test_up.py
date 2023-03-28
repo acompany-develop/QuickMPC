e8de72fedf5e74f91e5aebcf7c2589ecc96db067
Nakata <ryosuke.nakata@acompany-ac.com>
2023-03-28 20:22:53 +0900

    Remove envoy from up test

--- scripts/container_test/tests/test_up.py
+++ scripts/container_test/tests/test_up.py
@@ -9,15 +9,9 @@ from utils import init
         (Container("dev_unit_cc1")),
         (Container("dev_unit_cc2")),
         (Container("dev_unit_cc3")),
-        (Container("dev_unit_cc_envoy1")),
-        (Container("dev_unit_cc_envoy2")),
-        (Container("dev_unit_cc_envoy3")),
         (Container("dev_unit_mc1")),
         (Container("dev_unit_mc2")),
         (Container("dev_unit_mc3")),
-        (Container("dev_unit_mc_envoy1")),
-        (Container("dev_unit_mc_envoy2")),
-        (Container("dev_unit_mc_envoy3")),
         (Container("dev_unit_bts")),
     ]
 )
