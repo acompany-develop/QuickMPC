436342b81b4f038257e9f52faae8e2be8dd752b4
Nakata <ryosuke.nakata@acompany-ac.com>
2023-03-31 00:52:56 +0900

    Fix Container logic

--- scripts/container_test/tests/container.py
+++ scripts/container_test/tests/container.py
@@ -16,63 +16,42 @@ compose_files_opt = " ".join(
 
 
 @dataclass(frozen=True)
-class Container:
-    __service_name: str
+class Containers:
+    __service_names: List[str]
 
     __healthcheck_interval: ClassVar[int] = 3
     __healthcheck_num: ClassVar[int] = 10
 
-    def __container_id(self) -> str:
-        command = f"docker-compose {compose_files_opt} " + \
-            f"ps -q {self.__service_name}"
+    @staticmethod
+    def __container_id(service_name: str) -> str:
+        command = f"docker-compose {compose_files_opt} ps -q {service_name}"
         container_id = subprocess.check_output([command], shell=True)
-        return container_id.decode("utf-8")
+        return container_id.decode("utf-8")[:-1]
 
-    def __healthcheck(self) -> bool:
-        command = "docker inspect --format ='{{json .State.Health}}' " + \
-            f"{self.__container_id()}"
-        row = subprocess.check_output([command], shell=True)
-        json_log = json.loads(row.decode("utf-8")[1:])
-        return json_log["Status"] == "healthy"
+    @staticmethod
+    def __healthcheck(service_name: str) -> bool:
+        cid: str = Containers.__container_id(service_name)
+        command = "docker inspect --format ='{{json .State.Health}}' " f"{cid}"
+        for _ in range(Containers.__healthcheck_num):
+            row = subprocess.check_output([command], shell=True)
+            json_log = json.loads(row.decode("utf-8")[1:])
+            # NOTE: health checkが導入されてないコンテナもTrueとする
+            if not json_log or json_log["Status"] == "healthy":
+                return True
+            time.sleep(Containers.__healthcheck_interval)
+        return False
 
     def up(self) -> None:
-        command = f"docker-compose {compose_files_opt} " + \
-            f"up -d {self.__service_name}"
+        sn_str = " ".join(self.__service_names)
+        command = f"docker-compose {compose_files_opt} up -d {sn_str}"
         subprocess.run([command], shell=True)
         # healthcheckが通るまで待機，timeoutを過ぎたらraise
-        for _ in range(self.__healthcheck_num):
-            if self.__healthcheck():
-                return
-            time.sleep(self.__healthcheck_interval)
-        self.down()
-        raise RuntimeError(f"`{self.__service_name}` is not healthy.")
+        for sn in self.__service_names:
+            if not Containers.__healthcheck(sn):
+                self.down()
+                raise RuntimeError(f"`{sn}` is not healthy.")
 
     def down(self) -> None:
-        cid = self.__container_id()
-        if not cid:
-            # コンテナが立ってなかったらdownしない
-            return
-        command = f"docker-compose {compose_files_opt} " + \
-            f"rm -fs {self.__service_name}"
+        sn_str = " ".join(self.__service_names)
+        command = f"docker-compose {compose_files_opt} rm -fs {sn_str}"
         subprocess.run([command], shell=True)
-
-
-@dataclass(frozen=True)
-class Containers:
-    service_names: InitVar[List[str]]
-
-    __containers: List[Container] = field(init=False)
-
-    def __post_init__(self, service_names: List[str]) -> None:
-        containers = [Container(name) for name in service_names]
-        object.__setattr__(self, "_Containers__containers", containers)
-
-    def up(self) -> None:
-        with ProcessPoolExecutor() as executor:
-            for c in self.__containers:
-                executor.submit(c.up)
-
-    def down(self) -> None:
-        with ProcessPoolExecutor() as executor:
-            for c in self.__containers:
-                executor.submit(c.down)
--- scripts/container_test/tests/test_up.py
+++ scripts/container_test/tests/test_up.py
@@ -1,18 +1,18 @@
 import pytest
 
-from container import Container
+from container import Containers
 from utils import all_containers
 
 
 @pytest.mark.parametrize(
     ("container"), [
-        (Container("dev_unit_cc1")),
-        (Container("dev_unit_cc2")),
-        (Container("dev_unit_cc3")),
-        (Container("dev_unit_mc1")),
-        (Container("dev_unit_mc2")),
-        (Container("dev_unit_mc3")),
-        (Container("dev_unit_bts")),
+        (Containers(["dev_unit_cc1"])),
+        (Containers(["dev_unit_cc2"])),
+        (Containers(["dev_unit_cc3"])),
+        (Containers(["dev_unit_mc1"])),
+        (Containers(["dev_unit_mc2"])),
+        (Containers(["dev_unit_mc3"])),
+        (Containers(["dev_unit_bts"])),
     ]
 )
 def test_up(container):
