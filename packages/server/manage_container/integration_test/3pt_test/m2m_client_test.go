f824f7e7b875f6e3a25a80f3c036ebc75719add2
root <kota.takahashi@acompany-ac.com>
2023-04-06 06:58:18 +0000

    Add deleteStatusFile integration test

--- packages/server/manage_container/integration_test/3pt_test/m2m_client_test.go
+++ packages/server/manage_container/integration_test/3pt_test/m2m_client_test.go
@@ -136,3 +136,49 @@ func TestCreateStatusFile(t *testing.T) {
 	}
 	deleteId(t, jobUUID)
 }
+
+func TestDeleteStatusFile(t *testing.T) {
+	/**
+	* 各手順ごとにsyncで同期
+	* 1. PT2,3がstatu_RECEIVEDを作成
+	* 2. PT1が削除リクエストを送信
+	* 3. PT2,3で削除されたか確認
+	 */
+	jobUUID := "deleteStatusFile"
+	deleteId(t, jobUUID)
+
+	config, err := utils.GetConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+	id := config.PartyID
+	m2m_client := m2m.Client{}
+	if id == 1 {
+		// NOTE: PT1はPT2,3へstatu_RECEIVED削除リクエストを送る
+		m2m_client.Sync("create")
+		client := m2m.Client{}
+		err := client.DeleteStatusFile(jobUUID)
+		if err != nil {
+			t.Fatal(err)
+		}
+		// data削除の終了を通知
+		m2m_client.Sync("deleteStatusFile")
+	} else {
+		// NOTE: PT2,3はstatu_RECEIVED作成リクエストを受け取る
+		// PT2,3はstatu_RECEIVEDを作成する
+		path := fmt.Sprintf("/db/result/%s", jobUUID)
+		os.Mkdir(path, 0777)
+		fp, _ := os.Create(fmt.Sprintf("%s/status_RECEIVED", path))
+		fp.Close()
+		// 作成完了を通知
+		m2m_client.Sync("create")
+		// PT1からの削除リクエストの終了を待機
+		m2m_client.Sync("deleteStatusFile")
+		// 同期させてから削除されてるかチェック
+		_, err = os.Stat(fmt.Sprintf("/db/result/%s", jobUUID))
+		if err == nil {
+			t.Fatal(fmt.Sprintf("%s directory must be deleted", jobUUID))
+		}
+	}
+	deleteId(t, jobUUID)
+}
