29fb0a827b0e1ab938a9cd76b5f75341bfc7f994
Kota Takahashi <91713065+KotaTakahashi9320@users.noreply.github.com>
2023-04-14 19:03:37 +0900

    Fix setup.sh

--- demo/deploy/setup.sh
+++ demo/deploy/setup.sh
@@ -15,9 +15,6 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 sudo apt-get install --allow-downgrades docker-ce=5:20.10.17~3-0~ubuntu-focal docker-ce-cli=5:20.10.17~3-0~ubuntu-focal containerd.io docker-compose-plugin -y
-sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
 # docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
