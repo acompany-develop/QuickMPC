f65f51f7d7552fa3cb8a68512b69a3b7df267296
Sakamoto Souta <sota.sakamoto@acompany-ac.com>
2023-04-17 09:45:42 +0000

    ADD: function for v0

--- packages/server/manage_container/utils/convert_grpc_req.go
+++ packages/server/manage_container/utils/convert_grpc_req.go
@@ -2,10 +2,25 @@ package utils
 
 import (
 	pb_l2m "github.com/acompany-develop/QuickMPC/proto/libc_to_manage_container"
+	pb_l2m_v0 "github.com/acompany-develop/QuickMPC/proto/libc_to_manage_container_v0"
 	pb_m2c "github.com/acompany-develop/QuickMPC/proto/manage_to_computation_container"
 )
 
 func ConvertExecuteComputationRequest(in *pb_l2m.ExecuteComputationRequest, jobUUID string) *pb_m2c.ExecuteComputationRequest {
+	out := &pb_m2c.ExecuteComputationRequest{
+		MethodId: in.GetMethodId(),
+		JobUuid:  jobUUID,
+		Table: &pb_m2c.JoinOrder{
+			DataIds: in.GetTable().GetDataIds(),
+			Join:    in.GetTable().GetJoin(),
+			Index:   in.GetTable().GetIndex()},
+		Arg: &pb_m2c.Input{
+			Src:    in.GetArg().GetSrc(),
+			Target: in.GetArg().GetTarget()}}
+	return out
+}
+
+func ConvertExecuteComputationRequest_v0(in *pb_l2m_v0.ExecuteComputationRequest, jobUUID string) *pb_m2c.ExecuteComputationRequest {
 	out := &pb_m2c.ExecuteComputationRequest{
 		MethodId: in.GetMethodId(),
 		JobUuid:  jobUUID,
