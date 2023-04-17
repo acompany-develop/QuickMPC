25b4ce29dfd4aaeb2f9957d83244ec23359f4183
Sakamoto Souta <sota.sakamoto@acompany-ac.com>
2023-04-17 06:58:42 +0000

    FIX: add server for v0

--- packages/client/libclient-py/tests/unit_tests/conftest.py
+++ packages/client/libclient-py/tests/unit_tests/conftest.py
@@ -8,6 +8,7 @@ import os
 import pytest
 
 from .local_server import serve
+from .local_server_v0 import serve_v0
 
 
 @pytest.fixture(scope='session')
@@ -43,6 +44,48 @@ def run_server2():
 def run_server3():
     """ run server """
     server = serve(3)
+    server.start()
+
+    """ test start """
+    yield
+
+    """ test end """
+    server.stop(0)
+
+
+@pytest.fixture(scope='session')
+def run_server4():
+    """ run server """
+    server = serve_v0(4)
+    server.start()
+
+    """ test start """
+    yield
+
+    """ test end """
+    for file in glob.glob("./tests/unit_tests/dim1-uuid*"):
+        os.remove(file)
+
+    server.stop(0)
+
+
+@pytest.fixture(scope='session')
+def run_server5():
+    """ run server """
+    server = serve_v0(5)
+    server.start()
+
+    """ test start """
+    yield
+
+    """ test end """
+    server.stop(0)
+
+
+@pytest.fixture(scope='session')
+def run_server6():
+    """ run server """
+    server = serve_v0(6)
     server.start()
 
     """ test start """
