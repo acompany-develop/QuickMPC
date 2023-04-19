4888b92fdd933cac3a1707cbe7b02652d4c0ca71
Sakamoto Souta <sota.sakamoto@acompany-ac.com>
2023-04-19 02:33:15 +0000

    FIX: use send_share_from_csv

--- scripts/libclient/src/tests/test_send_share.py
+++ scripts/libclient/src/tests/test_send_share.py
@@ -6,33 +6,33 @@ import pytest
 from utils import qmpc
 
 
-def send_share_param(secrets=[[1, 2, 3], [4, 5, 6]],
-                     schema=["a", "b", "c"],
-                     matching_column=1,
-                     piece_size=1_000_000):
-    return (secrets, schema, matching_column, piece_size)
+def csv_data_param(secrets=[[1, 2, 3], [4, 5, 6]],
+                   schema=["a", "b", "c"],
+                   matching_column=1,
+                   piece_size=1_000_000):
+    return ([schema]+secrets, matching_column, piece_size)
 
 
 @pytest.mark.parametrize(
     ("param"),
     [
         # usually case
-        (send_share_param()),
+        (csv_data_param()),
 
         # small table size case
-        (send_share_param(secrets=[[1]], schema=["a"])),
+        (csv_data_param(secrets=[[1]], schema=["a"])),
 
         # nonzero matching_column case
-        (send_share_param(matching_column=3)),
+        (csv_data_param(matching_column=3)),
 
         # small piece_size case
-        (send_share_param(piece_size=1_000)),
+        (csv_data_param(piece_size=1_000)),
     ]
 )
-def test_send_share(param: tuple):
+def test_send_share_from_csv_data(param: tuple):
 
     # データをシェア化し送信する
-    res = qmpc.send_share(*param)
+    res = qmpc.send_share_from_csv_data(*param)
     assert (res["is_ok"])
 
     # 冪等性のために消しておく
