10f6ce66ef7693401cbcea63a01002b93fdaf85c
Tomonori HIRATA <hirata@sqlab.jp>
2023-04-30 21:31:29 +0900

    feat: introduce monitoring systems

--- scripts/demo/full_debug.sh
+++ scripts/demo/full_debug.sh
@@ -3,7 +3,7 @@
 
 # build処理を記述する関数
 build() {
-    docker buildx bake $COMPOSE_FILES_OPT dev_bts dev_cc1 dev_cc2 dev_cc3 dev_mc1 dev_mc2 dev_mc3 --load
+    docker buildx bake $COMPOSE_FILES_OPT dev_bts dev_cc1 dev_cc2 dev_cc3 dev_mc1 dev_mc2 dev_mc3 grafana --load
 }
 
 # runの前に実行されるsetup処理を記述する関数
@@ -16,7 +16,7 @@ setup() {
 # NOTE: この関数は例外的にワンライナーで書かなくて良い
 run() {
     docker-compose $COMPOSE_FILES_OPT up -d dev_cc_envoy1 dev_cc_envoy2 dev_cc_envoy3 dev_mc_envoy1 dev_mc_envoy2 dev_mc_envoy3
-    docker-compose $COMPOSE_FILES_OPT up dev_bts dev_cc1 dev_cc2 dev_cc3 dev_mc1 dev_mc2 dev_mc3
+    docker-compose $COMPOSE_FILES_OPT up dev_bts dev_cc1 dev_cc2 dev_cc3 dev_mc1 dev_mc2 dev_mc3 grafana
 }
 
 # runの後に実行されるteardown処理を記述する関数
