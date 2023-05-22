24faa6c371e049bfb03b69f80f88a3409a207896
Kota Takahashi <91713065+KotaTakahashi9320@users.noreply.github.com>
2023-05-22 16:44:55 +0900

    Fix to be able to deploy in n-party

--- demo/deploy/prepare_deploy.sh
+++ demo/deploy/prepare_deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# ./prepare_deploy.sh party_num party1_ip party2_ip bts_ip image_tag token
+# ./prepare_deploy.sh party_num party_str image_tag
 party_num=$1
-((party_num++))
-party1_ip=$2
-party2_ip=$3
-bts_ip=$4
-image_tag=$5
-token=$6
+party_str=$2
+image_tag=$3
 
+((party_num++))
+party_list=(${party_str//,/ })
+bts_ip=${party_list[(${#party_list[@]} - 1)]}
+n_parties=$((${#party_list[@]} - 1))
 readonly CC_PATH="./config/computation_container"
 readonly MC_PATH="./config/manage_container"
 readonly BTS_PATH="./config/beaver_triple_service"
@@ -18,48 +18,37 @@ search="{tag}"
 output_file="./docker-compose.yml"
 sed "s/${search}/${image_tag}/g" "${template_file}" > "${output_file}"
 
-if  [ "$party_num" != "3" ]; then
+if  [ $party_num -le $n_parties ]; then
     ## CCのconfig作成
     printf 'PARTY_ID=%s
-N_PARTIES=2
+N_PARTIES=%s
 SP_ID=1
 MC_TO_CC=http://0.0.0.0:50010
 CC_TO_BTS=http://%s:54000
 PORT_FOR_JOB=51020
-PARTY_LIST1=http://%s:50020
-PARTY_LIST2=http://%s:50020
-GETSHARE_TIME_LIMIT=50
-' "${party_num}" "${bts_ip}" "${party1_ip}" "${party2_ip}" > $CC_PATH/.env
+GETSHARE_TIME_LIMIT=50\n' "${party_num}" "${n_parties}" "${bts_ip}" > $CC_PATH/.env
 
-    ## MCのconfig作成
-    allowedOrigin=""
-    LF=$'\n'
-    for i in $(seq 0 $(($length - 1))); do
-    item=$(echo $jsonData | jq -r .allowedOrigin[${i}])
-    allowedOrigin+="\"${item}\""
-    if [ $i != $(($length - 1)) ]; then
-        allowedOrigin+=",${LF}      "
-    fi
+    for ((i=1; i<=n_parties; i++)); do
+        printf 'PARTY_LIST%s=http://%s:50020\n' "${i}" "${party_list[$((i-1))]}" >> $CC_PATH/.env
     done
 
     #　MCのconfig作成
-    PARTY_LIST1_PORT=$(( party_num ==1 ? 51011 : 50010  ))
-    PARTY_LIST2_PORT=$(( party_num ==1 ? 50010 : 51011  ))
     printf 'PARTY_ID=%s
-N_PARTIES=2
+N_PARTIES=%s
 MANAGE=http://localhost:50011
 COMPUTATION=http://computation_container:50010
-PARTY_LIST1=http://%s:%s
-PARTY_LIST2=http://%s:%s
 ALLOWEDORIGIN=http://localhost:8080
-' "${party_num}" "${party1_ip}" "${PARTY_LIST1_PORT}" "${party2_ip}" "${PARTY_LIST2_PORT}">$MC_PATH/.env
+' "${party_num}" "${n_parties}" >$MC_PATH/.env
 
+    for ((i=1; i<=n_parties; i++)); do
+        PORT=$(( ${i} == ${party_num} ? 51011 : 50010  ))
+        printf 'PARTY_LIST%s=http://%s:%s\n' "${i}" "${party_list[$((i-1))]}" "${PORT}" >> $MC_PATH/.env
+    done
     printf 'IS_BTS=false' > ./.env
 else
     # jwt作成
-    for i in {0..1}
+    for ((i=1; i<=n_parties; i++));
     do
-        num=$((i + 1))
         printf '# registered claims is following
 # "Expiration Time" represented by unix time
 # `github.com/golang-jwt/jwt/v4` v4.4.2 package will parse this as `float64`,
@@ -69,15 +58,15 @@ exp: 9223371974719179007
 # user defined claims is folloing
 room_uuid: '' # this field is filled by generator
 party_id: %s
-party_info:
-- id: 1
-  address: %s
-- id: 2
-  address: %s
-' "${num}" "${party1_ip}" "${party2_ip}">$BTS_PATH/sample.yaml
+party_info:' "${i}" > $BTS_PATH/sample.yaml
 
+        for ((j=1; j<=n_parties; j++)); do
+            printf '
+  - id: %s
+    address: %s' "${j}" "${party_list[$((j-1))]}" >> $BTS_PATH/sample.yaml
+        done
         make gen-jwt
-        cp $BTS_PATH/client.sample.env $BTS_PATH/client$i.sample.env
+        cp $BTS_PATH/client.sample.env $BTS_PATH/client$((i-1)).sample.env
     done
     printf 'IS_BTS=true' > ./.env
 fi
--- demo/terraform/gcp/deploy.tf
+++ demo/terraform/gcp/deploy.tf
@@ -1,9 +1,7 @@
 locals {
   party_num = var.instance_count - 1
   bts_num = var.instance_count - 1
-  ip1 = google_compute_instance.qmpc_k8s_vm[0].network_interface.0.access_config.0.nat_ip
-  ip2 = google_compute_instance.qmpc_k8s_vm[1].network_interface.0.access_config.0.nat_ip
-  ip3 = google_compute_instance.qmpc_k8s_vm[2].network_interface.0.access_config.0.nat_ip
+  ip_str = join(",", google_compute_instance.qmpc_k8s_vm.*.network_interface.0.access_config.0.nat_ip)
 }
 
 # ---------------------------
@@ -46,7 +44,7 @@ resource "null_resource" "setup" {
         ]
     }
 
-    depends_on = [null_resource.copy_file]
+    depends_on = [google_compute_firewall.qmpc_k8s_fw]
 }
 
 # ---------------------------
@@ -61,7 +59,7 @@ resource "null_resource" "create_jwt" {
             private_key = file("${local_file.private_key_pem.filename}")
         }
         inline = [
-            "cd /home/${var.gce_ssh_user}/QuickMPC && chmod 777 ./prepare_deploy.sh && ./prepare_deploy.sh ${local.bts_num} ${local.ip1} ${local.ip2} ${local.ip3} ${var.docker_image_tag}"
+            "cd /home/${var.gce_ssh_user}/QuickMPC && chmod +x ./prepare_deploy.sh && ./prepare_deploy.sh ${local.bts_num} ${local.ip1} ${local.ip2} ${local.ip3} ${var.docker_image_tag}"
         ]
     }
     depends_on = [null_resource.setup]
@@ -83,16 +81,10 @@ resource "null_resource" "get_jwt" {
 # ---------------------------
 # sent jwt
 # ---------------------------
-resource "null_resource" "sent_jwt_to_1" {
+resource "null_resource" "sent_jwt" {
+    count        = local.party_num
     provisioner "local-exec" {
-        command = "scp -oStrictHostKeyChecking=no -i ${local_file.private_key_pem.filename} ./client0.sample.env ${var.gce_ssh_user}@${local.ip1}:/home/${var.gce_ssh_user}/QuickMPC/config/beaver_triple_service/client.sample.env"
-        interpreter = ["bash", "-c"]
-    }
-    depends_on = [null_resource.get_jwt]
-}
-resource "null_resource" "sent_jwt_to_2" {
-    provisioner "local-exec" {
-        command = "scp -oStrictHostKeyChecking=no -i ${local_file.private_key_pem.filename} ./client1.sample.env ${var.gce_ssh_user}@${local.ip2}:/home/${var.gce_ssh_user}/QuickMPC/config/beaver_triple_service/client.sample.env"
+        command = "scp -oStrictHostKeyChecking=no -i ${local_file.private_key_pem.filename} ./client${count.index}.sample.env ${var.gce_ssh_user}@${google_compute_instance.qmpc_k8s_vm[count.index].network_interface.0.access_config.0.nat_ip}:/home/${var.gce_ssh_user}/QuickMPC/config/beaver_triple_service/client.sample.env"
         interpreter = ["bash", "-c"]
     }
     depends_on = [null_resource.get_jwt]
