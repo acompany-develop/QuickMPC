5390d376c8adff256f888ba7601c0d896beb60c9
Kota Takahashi <91713065+KotaTakahashi9320@users.noreply.github.com>
2023-05-22 16:45:41 +0900

    Fix common processing to be done at the same time

--- demo/terraform/gcp/deploy.tf
+++ demo/terraform/gcp/deploy.tf
@@ -7,7 +7,7 @@ locals {
 # ---------------------------
 # set deploy directory
 # ---------------------------
-resource "null_resource" "copy_file" {
+resource "null_resource" "setup" {
     count        = var.instance_count
 
     provisioner "file" {
@@ -21,15 +21,6 @@ resource "null_resource" "copy_file" {
         }
     }
 
-    depends_on = [google_compute_instance.qmpc_k8s_vm]
-}
-
-
-# ---------------------------
-# setup
-# ---------------------------
-resource "null_resource" "setup" {
-    count = var.instance_count
     provisioner "remote-exec" {
         connection {
             host        = google_compute_instance.qmpc_k8s_vm[count.index].network_interface.0.access_config.0.nat_ip
@@ -48,18 +39,19 @@ resource "null_resource" "setup" {
 }
 
 # ---------------------------
-# create JWT
+# Prepare deploy
 # ---------------------------
-resource "null_resource" "create_jwt" {
+resource "null_resource" "prepare_deploy" {
+    count        = var.instance_count
     provisioner "remote-exec" {
         connection {
-            host        = google_compute_instance.qmpc_k8s_vm[local.bts_num].network_interface.0.access_config.0.nat_ip
+            host        = google_compute_instance.qmpc_k8s_vm[count.index].network_interface.0.access_config.0.nat_ip
             type        = "ssh"
             user        = "${var.gce_ssh_user}"
             private_key = file("${local_file.private_key_pem.filename}")
         }
         inline = [
-            "cd /home/${var.gce_ssh_user}/QuickMPC && chmod +x ./prepare_deploy.sh && ./prepare_deploy.sh ${local.bts_num} ${local.ip1} ${local.ip2} ${local.ip3} ${var.docker_image_tag}"
+            "cd /home/${var.gce_ssh_user}/QuickMPC && chmod +x ./prepare_deploy.sh && ./prepare_deploy.sh ${count.index} ${local.ip_str} ${var.docker_image_tag}"
         ]
     }
     depends_on = [null_resource.setup]
@@ -70,12 +62,11 @@ resource "null_resource" "create_jwt" {
 # get jwt
 # ---------------------------
 resource "null_resource" "get_jwt" {
-    count        = local.party_num
     provisioner "local-exec" {
-        command = "scp -oStrictHostKeyChecking=no -i ${local_file.private_key_pem.filename} ${var.gce_ssh_user}@${local.ip3}:/home/${var.gce_ssh_user}/QuickMPC/config/beaver_triple_service/client${count.index}.sample.env ./client${count.index}.sample.env"
+        command = "scp -oStrictHostKeyChecking=no -i ${local_file.private_key_pem.filename} ${var.gce_ssh_user}@${google_compute_instance.qmpc_k8s_vm[local.bts_num].network_interface.0.access_config.0.nat_ip}:/home/${var.gce_ssh_user}/QuickMPC/config/beaver_triple_service/client?*.sample.env ./"
         interpreter = ["bash", "-c"]
     }
-    depends_on = [null_resource.create_jwt]
+    depends_on = [null_resource.prepare_deploy]
 }
 
 # ---------------------------
@@ -88,29 +79,6 @@ resource "null_resource" "sent_jwt" {
         interpreter = ["bash", "-c"]
     }
     depends_on = [null_resource.get_jwt]
-}
-
-# ---------------------------
-# set config
-# ---------------------------
-
-resource "null_resource" "set_config" {
-    count = local.party_num
-
-    provisioner "remote-exec" {
-        connection {
-            host        = google_compute_instance.qmpc_k8s_vm[count.index].network_interface.0.access_config.0.nat_ip
-            type        = "ssh"
-            user        = "${var.gce_ssh_user}"
-            private_key = file("${local_file.private_key_pem.filename}")
-        }
-
-        inline = [
-            "cd /home/${var.gce_ssh_user}/QuickMPC && chmod 777 ./prepare_deploy.sh && ./prepare_deploy.sh ${count.index} ${local.ip1} ${local.ip2} ${local.ip3} ${var.docker_image_tag}"
-        ]
-    }
-
-    depends_on = [null_resource.sent_jwt_to_1,null_resource.sent_jwt_to_2]
 }
 
 # ---------------------------
@@ -130,5 +98,5 @@ resource "null_resource" "deploy_quickmpc" {
             "make upd"
         ]
     }
-    depends_on = [null_resource.set_config]
+    depends_on = [null_resource.sent_jwt]
 }
