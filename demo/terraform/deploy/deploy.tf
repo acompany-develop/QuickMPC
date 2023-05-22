ee322d6cf8a2e45649e4404450b74f3feaf85ac9
Kota Takahashi <91713065+KotaTakahashi9320@users.noreply.github.com>
2023-05-22 21:19:28 +0900

    Modify to use values retrieved from json

--- demo/terraform/deploy/deploy.tf
+++ demo/terraform/deploy/deploy.tf
@@ -1,57 +1,55 @@
 locals {
-  party_num = var.instance_count - 1
-  bts_num = var.instance_count - 1
-  ip_str = join(",", google_compute_instance.qmpc_k8s_vm.*.network_interface.0.access_config.0.nat_ip)
+  party_num = local.instance_count - 1
+  bts_num = local.instance_count - 1
+  ip_str = join(",", local.qmpc_instance_gips)
 }
 
 # ---------------------------
 # set deploy directory
 # ---------------------------
 resource "null_resource" "setup" {
-    count        = var.instance_count
+    count        = local.instance_count
 
     provisioner "file" {
         source      = "../../deploy"
-        destination = "/home/${var.gce_ssh_user}/QuickMPC"
+        destination = "/home/${local.gce_ssh_user}/QuickMPC"
         connection {
-            host        = google_compute_instance.qmpc_k8s_vm[count.index].network_interface.0.access_config.0.nat_ip
+            host        = local.qmpc_instance_gips[count.index]
             type        = "ssh"
-            user        = "${var.gce_ssh_user}"
-            private_key = file("${local_file.private_key_pem.filename}")
+            user        = "${local.gce_ssh_user}"
+            private_key = file("${local.private_key_path}")
         }
     }
 
     provisioner "remote-exec" {
         connection {
-            host        = google_compute_instance.qmpc_k8s_vm[count.index].network_interface.0.access_config.0.nat_ip
+            host        = local.qmpc_instance_gips[count.index]
             type        = "ssh"
-            user        = "${var.gce_ssh_user}"
-            private_key = file("${local_file.private_key_pem.filename}")
+            user        = "${local.gce_ssh_user}"
+            private_key = file("${local.private_key_path}")
         }
 
         inline = [
-            "cd /home/${var.gce_ssh_user}/QuickMPC",
+            "cd /home/${local.gce_ssh_user}/QuickMPC",
             "sh setup.sh",
         ]
     }
-
-    depends_on = [google_compute_firewall.qmpc_k8s_fw]
 }
 
 # ---------------------------
 # Prepare deploy
 # ---------------------------
 resource "null_resource" "prepare_deploy" {
-    count        = var.instance_count
+    count        = local.instance_count
     provisioner "remote-exec" {
         connection {
-            host        = google_compute_instance.qmpc_k8s_vm[count.index].network_interface.0.access_config.0.nat_ip
+            host        = local.qmpc_instance_gips[count.index]
             type        = "ssh"
-            user        = "${var.gce_ssh_user}"
-            private_key = file("${local_file.private_key_pem.filename}")
+            user        = "${local.gce_ssh_user}"
+            private_key = file("${local.private_key_path}")
         }
         inline = [
-            "cd /home/${var.gce_ssh_user}/QuickMPC && chmod +x ./prepare_deploy.sh && ./prepare_deploy.sh ${count.index} ${local.ip_str} ${var.docker_image_tag}"
+            "cd /home/${local.gce_ssh_user}/QuickMPC && chmod +x ./prepare_deploy.sh && ./prepare_deploy.sh ${count.index} ${local.ip_str} ${var.docker_image_tag}"
         ]
     }
     depends_on = [null_resource.setup]
@@ -62,7 +60,7 @@ resource "null_resource" "prepare_deploy" {
 # ---------------------------
 resource "null_resource" "get_jwt" {
     provisioner "local-exec" {
-        command = "scp -oStrictHostKeyChecking=no -i ${local_file.private_key_pem.filename} ${var.gce_ssh_user}@${google_compute_instance.qmpc_k8s_vm[local.bts_num].network_interface.0.access_config.0.nat_ip}:/home/${var.gce_ssh_user}/QuickMPC/config/beaver_triple_service/client?*.sample.env ./"
+        command = "scp -oStrictHostKeyChecking=no -i ${local.private_key_path} ${local.gce_ssh_user}@${local.qmpc_instance_gips[local.bts_num]}:/home/${local.gce_ssh_user}/QuickMPC/config/beaver_triple_service/client?*.sample.env ./"
         interpreter = ["bash", "-c"]
     }
     depends_on = [null_resource.prepare_deploy]
@@ -74,7 +72,7 @@ resource "null_resource" "get_jwt" {
 resource "null_resource" "sent_jwt" {
     count        = local.party_num
     provisioner "local-exec" {
-        command = "scp -oStrictHostKeyChecking=no -i ${local_file.private_key_pem.filename} ./client${count.index}.sample.env ${var.gce_ssh_user}@${google_compute_instance.qmpc_k8s_vm[count.index].network_interface.0.access_config.0.nat_ip}:/home/${var.gce_ssh_user}/QuickMPC/config/beaver_triple_service/client.sample.env"
+        command = "scp -oStrictHostKeyChecking=no -i ${local.private_key_path} ./client${count.index}.sample.env ${local.gce_ssh_user}@${local.qmpc_instance_gips[count.index]}:/home/${local.gce_ssh_user}/QuickMPC/config/beaver_triple_service/client.sample.env"
         interpreter = ["bash", "-c"]
     }
     depends_on = [null_resource.get_jwt]
@@ -84,16 +82,16 @@ resource "null_resource" "sent_jwt" {
 # deploy QuickMPC
 # ---------------------------
 resource "null_resource" "deploy_quickmpc" {
-    count = var.instance_count
+    count = local.instance_count
     provisioner "remote-exec" {
         connection {
-            host        = google_compute_instance.qmpc_k8s_vm[count.index].network_interface.0.access_config.0.nat_ip
+            host        = local.qmpc_instance_gips[count.index]
             type        = "ssh"
-            user        = "${var.gce_ssh_user}"
-            private_key = file("${local_file.private_key_pem.filename}")
+            user        = "${local.gce_ssh_user}"
+            private_key = file("${local.private_key_path}")
         }
         inline = [
-            "cd /home/${var.gce_ssh_user}/QuickMPC",
+            "cd /home/${local.gce_ssh_user}/QuickMPC",
             "make upd"
         ]
     }
