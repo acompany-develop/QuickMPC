9c2c910b22c157eece7ea2cd7e1efd271ddcf016
Kota Takahashi <91713065+KotaTakahashi9320@users.noreply.github.com>
2023-05-22 21:18:19 +0900

    Remove unnecessary variable

--- demo/terraform/deploy/vaiables.tf
+++ demo/terraform/deploy/vaiables.tf
@@ -4,7 +4,6 @@ locals {
   qmpc_instance_gips = local.json_data["qmpc_instance_gips"]["value"]
   private_key_path = local.json_data["private_key_path"]["value"]
   gce_ssh_user =  local.json_data["gce_ssh_user"]["value"]
-  docker_image_tag = "0.3.8"
 }
 
 variable "docker_image_tag" {
