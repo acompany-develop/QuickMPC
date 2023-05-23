40aaa5973db008337df2bb9826822528347c860f
Kota Takahashi <91713065+KotaTakahashi9320@users.noreply.github.com>
2023-05-23 20:30:04 +0900

    Add code description

--- demo/deploy/prepare_deploy.sh
+++ demo/deploy/prepare_deploy.sh
@@ -1,47 +1,57 @@
 #!/bin/bash
-# ./prepare_deploy.sh party_num party_str image_tag
-party_num=$1
-party_str=$2
-image_tag=$3
+# ./prepare_deploy.sh party_id party_str image_tag
+party_id=$1           # パーティの番号を引数から取得
+party_str=$2          # パーティのipリストを引数から取得
+image_tag=$3          # containerイメージのタグを引数から取得
 
-((party_num++))
+# 0-indexを1-indexに変換(terraformのcount.indexが0-indexのため引数は0-index)
+((party_id++))
+# パーティのリストをカンマで区切って配列に格納
 party_list=(${party_str//,/ })
+
+# party_listの最後の要素をbts_ipとして取得
 bts_ip=${party_list[(${#party_list[@]} - 1)]}
+
+# btsを除いたパーティの数を取得
 n_parties=$((${#party_list[@]} - 1))
+
+# 各configファイルの出力先を定義
 readonly CC_PATH="./config/computation_container"
 readonly MC_PATH="./config/manage_container"
 readonly BTS_PATH="./config/beaver_triple_service"
 
-# create docker-compose.yml
+# テンプレートファイル内の"{tag}"をimage_tagで置換し、出力ファイルを作成
 template_file="./docker-compose.yml.template"
 search="{tag}"
 output_file="./docker-compose.yml"
 sed "s/${search}/${image_tag}/g" "${template_file}" > "${output_file}"
 
-if  [ $party_num -le $n_parties ]; then
-    ## CCのconfig作成
+if [ $party_id -le $n_parties ]; then
+    # CCのconfig作成
     printf 'PARTY_ID=%s
 N_PARTIES=%s
 SP_ID=1
 MC_TO_CC=http://0.0.0.0:50010
 CC_TO_BTS=http://%s:54000
 PORT_FOR_JOB=51020
-GETSHARE_TIME_LIMIT=50\n' "${party_num}" "${n_parties}" "${bts_ip}" > $CC_PATH/.env
+GETSHARE_TIME_LIMIT=50\n' "${party_id}" "${n_parties}" "${bts_ip}" > $CC_PATH/.env
 
+    # 各パーティの情報をCCのconfigに追記
     for ((i=1; i<=n_parties; i++)); do
         printf 'PARTY_LIST%s=http://%s:50020\n' "${i}" "${party_list[$((i-1))]}" >> $CC_PATH/.env
     done
 
-    #　MCのconfig作成
+    # MCのconfig作成
     printf 'PARTY_ID=%s
 N_PARTIES=%s
 MANAGE=http://localhost:50011
 COMPUTATION=http://computation_container:50010
 ALLOWEDORIGIN=http://localhost:8080
-' "${party_num}" "${n_parties}" >$MC_PATH/.env
+' "${party_id}" "${n_parties}" >$MC_PATH/.env
 
+    # 各パーティの情報をMCのconfigに追記
     for ((i=1; i<=n_parties; i++)); do
-        PORT=$(( ${i} == ${party_num} ? 51011 : 50010  ))
+        PORT=$(( ${i} == ${party_id} ? 51011 : 50010  ))
         printf 'PARTY_LIST%s=http://%s:%s\n' "${i}" "${party_list[$((i-1))]}" "${PORT}" >> $MC_PATH/.env
     done
     printf 'IS_BTS=false' > ./.env
@@ -60,6 +70,7 @@ room_uuid: '' # this field is filled by generator
 party_id: %s
 party_info:' "${i}" > $BTS_PATH/sample.yaml
 
+        # 各パーティの情報をsample.yamlに追記
         for ((j=1; j<=n_parties; j++)); do
             printf '
   - id: %s
