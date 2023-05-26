5f1b1c2c4b5985231d5d5718fa60699cc4d71822
Sakamoto Souta <sota.sakamoto@acompany-ac.com>
2023-05-26 10:49:38 +0000

    decrease durations number

--- scripts/libclient/medium_test.sh
+++ scripts/libclient/medium_test.sh
@@ -14,7 +14,7 @@ setup() {
 # run処理を記述する関数
 # NOTE: この関数は例外的にワンライナーで書かなくて良い
 run() {
-    docker-compose $COMPOSE_FILES_OPT run medium-libc pytest src/tests -s -v -log-cli-level=DEBUG --durations=0
+    docker-compose $COMPOSE_FILES_OPT run medium-libc pytest src/tests -s -v -log-cli-level=DEBUG --durations=5
 }
 
 # runの後に実行されるteardown処理を記述する関数
