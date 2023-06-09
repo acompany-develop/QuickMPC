bad686571d3763961313a8222ce043222911df7a
KotaTakahashi9320 <kota.takahashi@acompany-ac.com>
2023-06-09 06:12:47 +0000

    Fix to output required information.

--- demo/terraform/application/output.tf
+++ demo/terraform/application/output.tf
@@ -5,3 +5,15 @@ output "qmpc_instance_gips" {
 output "qmpc_party_ip" {
   value = local.party_ip
 }
+
+output "party_size" {
+  value = var.party_size
+}
+
+output "private_key_path" {
+  value = local.private_key_path
+}
+
+output "gce_ssh_user" {
+  value = local.gce_ssh_user
+}
