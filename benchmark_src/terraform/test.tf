6c9a0439a5b76314f7e196369f553b9b7801d400
KotaTakahashi9320 <kota.takahashi@acompany-ac.com>
2023-06-09 06:42:07 +0000

    Add terraform file to run test

---
+++ benchmark_src/terraform/test.tf
@@ -0,0 +1,82 @@
+locals {
+  json_data = jsondecode(file("./output.json"))
+  party_size = local.json_data["party_size"]["value"]
+  qmpc_instance_gips = local.json_data["qmpc_instance_gips"]["value"]
+  private_key_path = "../../demo/terraform/application/${local.json_data["private_key_path"]["value"]}"
+  gce_ssh_user =  local.json_data["gce_ssh_user"]["value"]
+  party_num = local.party_size - 1
+  client_id = length(local.qmpc_instance_gips) - 1
+  party_ip = slice(local.qmpc_instance_gips, 0, local.party_num)
+  ip_str = join(",", local.party_ip)
+}
+
+# ---------------------------
+# benchmark
+# ---------------------------
+resource "null_resource" "benchmark" {
+    provisioner "file" {
+        source      = "../test"
+        destination = "/home/${local.gce_ssh_user}/test"
+        connection {
+            host        = local.qmpc_instance_gips[local.client_id]
+            type        = "ssh"
+            user        = "${local.gce_ssh_user}"
+            private_key = file("${local.private_key_path}")
+        }
+    }
+
+    provisioner "file" {
+        source      = "../../scripts/libclient/src"
+        destination = "/home/${local.gce_ssh_user}/test/src"
+        connection {
+            host        = local.qmpc_instance_gips[local.client_id]
+            type        = "ssh"
+            user        = "${local.gce_ssh_user}"
+            private_key = file("${local.private_key_path}")
+        }
+    }
+
+    provisioner "file" {
+        source      = "../../demo/terraform/gcp/json/service_account.json"
+        destination = "/home/${local.gce_ssh_user}/test/service_account.json"
+        connection {
+            host        = local.qmpc_instance_gips[local.client_id]
+            type        = "ssh"
+            user        = "${local.gce_ssh_user}"
+            private_key = file("${local.private_key_path}")
+        }
+    }
+
+    provisioner "remote-exec" {
+        connection {
+            host        = local.qmpc_instance_gips[local.client_id]
+            type        = "ssh"
+            user        = "${local.gce_ssh_user}"
+            private_key = file("${local.private_key_path}")
+        }
+
+        inline = [
+            "cd /home/${local.gce_ssh_user}/test",
+            "chmod +x ./prepare.sh && ./prepare.sh ${local.ip_str}",
+            "mkdir /home/${local.gce_ssh_user}/test/data",
+            "export GOOGLE_APPLICATION_CREDENTIALS=\"/home/${local.gce_ssh_user}/test/service_account.json\" && gcsfuse benchmark-ci-data /home/${local.gce_ssh_user}/test/data",
+        ]
+    }
+
+    provisioner "remote-exec" {
+        connection {
+            host        = local.qmpc_instance_gips[local.client_id]
+            type        = "ssh"
+            user        = "${local.gce_ssh_user}"
+            private_key = file("${local.private_key_path}")
+            timeout     = "24h"
+        }
+        inline = [
+            "cd /home/${local.gce_ssh_user}/test",
+            # NOTE: gcsをmountしたディレクトリがdockerにmountできなかったためディレクトリを作成している
+            # TODO 直接mountできるようにする
+            "mkdir ./test_data && cp ./data/* ./test_data",
+            "chmod +x tester.sh && ./tester.sh",
+        ]
+    }
+}
