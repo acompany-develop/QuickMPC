afef685ea38e97c2291cb94d8f004fba87929b18
KotaTakahashi9320 <kota.takahashi@acompany-ac.com>
2023-06-09 05:44:36 +0000

    Fix to get data from file

--- scripts/libclient/src/benchmark/test_statistics.py
+++ scripts/libclient/src/benchmark/test_statistics.py
@@ -1,10 +1,10 @@
 import math
+import csv
 
 import numpy as np
 import pytest
 from utils import get_result, qmpc
 
-from benchmark.data import large_data
 from benchmark.metrics import PrintTime
 
 
@@ -12,32 +12,34 @@ from benchmark.metrics import PrintTime
 def get_data_id():
     val: dict = {}
 
-    def get(size: int, data_num):
-        if (size, data_num) in val:
-            return val[(size, data_num)]
-        data = large_data(size, data_num)
+    def get(path: str):
+        if path in val:
+            return val[path]
         with PrintTime("send_share"):
-            res = qmpc.send_share_from_csv_data(data)
+            res = qmpc.send_share_from_csv_file(path)
         assert (res["is_ok"])
         data_id: str = res["data_id"]
-        schema_size: int = len(data[0])
-        val[(size, data_num)] = (data_id, schema_size)
-        return val[(size, data_num)]
-    return lambda size, data_num=1: get(size, data_num)
+        with open(path) as f:
+            reader = csv.reader(f)
+            row = next(reader)
+            schema_size: int = len(row)
+        val[path] = (data_id, schema_size)
+        return val[path]
+    return lambda path: get(path)
 
 
-# types: (iterate_num, size)
+# types: (iterate_num, path)
 test_parameters = [
-    (10, 5),
-    (10, 50)
+    (10, "data/table_data_10x10.csv"),
+    (10, "data/table_data_100x10.csv"),
 ]
 
 
 @pytest.mark.parametrize(
-    ("iterate_num", "size"), test_parameters
+    ("iterate_num", "path"), test_parameters
 )
-def test_mean(iterate_num: int, size: int, get_data_id):
-    data_id, schema_size = get_data_id(size)
+def test_mean(iterate_num: int, path: int, get_data_id):
+    data_id, schema_size = get_data_id(path)
     for _ in range(iterate_num):
         # table情報と列指定情報を定義して計算
         inp = [i+1 for i in range(schema_size)]
@@ -47,10 +49,10 @@ def test_mean(iterate_num: int, size: int, get_data_id):
 
 
 @pytest.mark.parametrize(
-    ("iterate_num", "size"), test_parameters
+    ("iterate_num", "path"), test_parameters
 )
-def test_sum(iterate_num: int, size: int, get_data_id):
-    data_id, schema_size = get_data_id(size)
+def test_sum(iterate_num: int, path: int, get_data_id):
+    data_id, schema_size = get_data_id(path)
     for _ in range(iterate_num):
         # table情報と列指定情報を定義して計算
         inp = [i+1 for i in range(schema_size)]
@@ -60,10 +62,10 @@ def test_sum(iterate_num: int, size: int, get_data_id):
 
 
 @pytest.mark.parametrize(
-    ("iterate_num", "size"), test_parameters
+    ("iterate_num", "path"), test_parameters
 )
-def test_variance(iterate_num: int, size: int, get_data_id):
-    data_id, schema_size = get_data_id(size)
+def test_variance(iterate_num: int, path: int, get_data_id):
+    data_id, schema_size = get_data_id(path)
     for _ in range(iterate_num):
         # table情報と列指定情報を定義して計算
         inp = [i+1 for i in range(schema_size)]
@@ -73,10 +75,10 @@ def test_variance(iterate_num: int, size: int, get_data_id):
 
 
 @pytest.mark.parametrize(
-    ("iterate_num", "size"), test_parameters
+    ("iterate_num", "path"), test_parameters
 )
-def test_correl(iterate_num: int, size: int, get_data_id):
-    data_id, schema_size = get_data_id(size)
+def test_correl(iterate_num: int, path: int, get_data_id):
+    data_id, schema_size = get_data_id(path)
     for _ in range(iterate_num):
         # table情報と列指定情報を定義して計算
         inp = ([i+1 for i in range(schema_size)], [schema_size])
@@ -86,14 +88,13 @@ def test_correl(iterate_num: int, size: int, get_data_id):
 
 
 @pytest.mark.parametrize(
-    ("iterate_num", "size"), test_parameters
+    ("iterate_num", "path"), test_parameters
 )
-def test_hjoin(iterate_num: int, size: int, get_data_id):
-    data_id1, _ = get_data_id(size, data_num=1)
-    data_id2, _ = get_data_id(size, data_num=2)
+def test_hjoin(iterate_num: int, path: int, get_data_id):
+    data_id, _ = get_data_id(path)
     for _ in range(iterate_num):
         # table情報を定義して計算
         with PrintTime("hjoin"):
             res, job_uuid = get_result(qmpc.get_join_table(
-                [data_id1, data_id2]), limit=10000)
+                [data_id, data_id]), limit=10000)
         assert (res["is_ok"])
