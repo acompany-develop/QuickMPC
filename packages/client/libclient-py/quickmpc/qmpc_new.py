3ed1521db4f802fe235016a0a6cb6e2c0ec9a2ab
Nakata <ryosuke.nakata@acompany-ac.com>
2023-06-23 16:36:00 +0900

    Add share_data_frame and new qmpc class

---
+++ packages/client/libclient-py/quickmpc/qmpc_new.py
@@ -0,0 +1,105 @@
+from dataclasses import dataclass, field, InitVar
+from typing import List, Union
+
+import numpy as np
+import pandas as pd
+
+from .qmpc_request import QMPCRequest
+from .request.qmpc_request_interface import QMPCRequestInterface
+from .share_data_frame import ShareDataFrame
+from .utils.overload_tools import Dim1, methoddispatch
+from .utils.parse_csv import to_float
+
+
+@dataclass(frozen=True)
+class QMPC:
+    """QuickMPCのdataのやり取りをするクラス
+
+    Attributes
+    ----------
+    arg: Union[List[str], QMPCRequestInterface]]
+        parties: List[str]
+            serverのIP
+        qmpc_request: QMPCRequestMock
+            qmpc serverに対してrequestを送るinterface
+    """
+
+    arg: InitVar[Union[List[str], QMPCRequestInterface]]
+
+    __qmpc_request: QMPCRequestInterface = field(init=False)
+
+    @methoddispatch()
+    def __post_init__(self, *args, **kwargs):
+        raise NotImplementedError("QMPCClientにサポートしていない値が与えられた．")
+
+    @__post_init__.register(Dim1)
+    def __post_init__default(self, parties: List[str]):
+        object.__setattr__(self, "_QMPC__qmpc_request",
+                           QMPCRequest(parties))
+
+    @__post_init__.register(QMPCRequest)
+    def __post_init__original(self, qmpc_request: QMPCRequest):
+        # mydispatchを改造してinterfaceでオーバーロードさせる
+        object.__setattr__(self, "_QMPC__qmpc_request", qmpc_request)
+
+    def read_csv(self, *args, index_col: str, **kwargs) -> pd.DataFrame:
+        """csvからテーブルデータを読み込む．
+
+        テーブル結合処理に用いる列がどの列かを`index_col`で指定する必要がある．
+        `index_col`以外の引数は全てpandasのread_csvと同じ．
+
+        Parameters
+        ----------
+        filepath_or_buffer: str, path object or file-like object
+            らしい
+        index_col: str
+            ID列としたいカラム名
+
+        Returns
+        ----------
+        pd.DataFrame
+            読み込んだテーブルデータ
+        """
+        df = pd.read_csv(*args, **kwargs)
+        # ID列を数値化
+        df[index_col] = df[index_col].map(lambda x: to_float(x))
+        # join時にQMPCのCC側でID列でsortできる様に、座圧を行いindexに設定しておく
+        df["original_index"] = df.index
+        df = df.sort_values(by=index_col)
+        df = df.reset_index(drop=True)
+        df = df.sort_values(by="original_index")
+        df = df.drop('original_index', axis=1)
+        df.set_index(index_col)
+        return df
+
+    def send_to(self, df: pd.DataFrame) -> ShareDataFrame:
+        """QuickMPCサーバにデータを送信する．
+
+        Parameters
+        ----------
+        df: df.DataFrame
+            送信するデータ
+
+        Returns
+        ----------
+        ShareDataFrame
+            QuickMPC形式のDataframe
+        """
+        # send_shareできる形式に変換
+        res = self.__qmpc_request.send_share(df)
+        return ShareDataFrame(res.data_id, self.__qmpc_request)
+
+    def load_from(self, data_id: str) -> ShareDataFrame:
+        """既に送信してあるデータを参照する．
+
+        Parameters
+        ----------
+        data_id: str
+            既に送信してあるデータのID
+
+        Returns
+        ----------
+        ShareDataFrame
+            QuickMPC形式のDataframe
+        """
+        return ShareDataFrame(data_id, self.__qmpc_request)
---
+++ packages/client/libclient-py/quickmpc/share_data_frame.py
@@ -0,0 +1,139 @@
+import logging
+from dataclasses import dataclass
+from enum import Enum
+from functools import update_wrapper
+from typing import Callable
+
+from .request.qmpc_request_interface import QMPCRequestInterface
+
+logger = logging.getLogger(__name__)
+
+
+class ShareDataFrameStatus(Enum):
+    OK = 1
+    EXECUTE = 2
+
+
+def _wait_execute_decorator(func: Callable) -> Callable:
+    """計算が終了するまで待機するデコレータ
+
+    Parameters
+    ----------
+    func: Callable
+        実行前に計算を待機させたいmethod
+
+    Returns
+    ----------
+    Callable
+        デコレータ関数
+    """
+
+    def wrapper(self: "ShareDataFrame", *args, **kwargs):
+        self._wait_execute()
+        func(self, *args, **kwargs)
+    update_wrapper(wrapper, func)
+    return wrapper
+
+
+@dataclass
+class ShareDataFrame:
+    """テーブルデータを管理するクラス
+
+    Attributes
+    ----------
+    __id: str
+        データのID
+    __qmpc_request: QMPCClientInterface
+        QuickMPCとの通信を担うClient
+    __is_result: bool
+        send由来のDataFrame(False)なのかexecute由来のDataFrameなのか(True)
+    __status: ShareDataFrameStatus
+        現在の状態
+    """
+
+    __id: str
+    __qmpc_request: QMPCRequestInterface
+    __is_result: bool = False
+    __status: ShareDataFrameStatus = ShareDataFrameStatus.OK
+
+    def _wait_execute(self):
+        if self.__status == ShareDataFrameStatus.EXECUTE:
+            logger.info("wait execute...")
+            # 実際は計算が終了するまで待機する
+            self.__status = ShareDataFrameStatus.OK
+
+    def join(self, other: "ShareDataFrame") -> "ShareDataFrame":
+        """テーブルデータを結合する．
+
+        inner_joinのみ．
+
+        Parameters
+        ----------
+        other: ShareDataFrame
+            結合したいDataFrame
+
+        Returns
+        ----------
+        Result
+            結合したDataFrameのResult
+        """
+        res = self.__qmpc_request.join([self.__id, other.__id])
+        return ShareDataFrame(res.job_uuid, self.__qmpc_request,
+                              True, ShareDataFrameStatus.EXECUTE)
+
+    def sum(self, columns: list) -> "ShareDataFrame":
+        res = self.__qmpc_request.sum([self.__id], columns)
+        return ShareDataFrame(res.job_uuid, self.__qmpc_request,
+                              True, ShareDataFrameStatus.EXECUTE)
+
+    def mean(self, columns: list) -> "ShareDataFrame":
+        res = self.__qmpc_request.mean([self.__id], columns)
+        return ShareDataFrame(res.job_uuid, self.__qmpc_request,
+                              True, ShareDataFrameStatus.EXECUTE)
+
+    def variance(self, columns: list) -> "ShareDataFrame":
+        res = self.__qmpc_request.variance([self.__id], columns)
+        return ShareDataFrame(res.job_uuid, self.__qmpc_request,
+                              True, ShareDataFrameStatus.EXECUTE)
+
+    def correl(self, columns1: list, columns2: list) -> "ShareDataFrame":
+        res = self.__qmpc_request.correl([self.__id], columns1, columns2)
+        return ShareDataFrame(res.job_uuid, self.__qmpc_request,
+                              True, ShareDataFrameStatus.EXECUTE)
+
+    def meshcode(self, list, columns1: list, columns2: list) \
+            -> "ShareDataFrame":
+        res = self.__qmpc_request.meshcode([self.__id], columns1, columns2)
+        return ShareDataFrame(res.job_uuid, self.__qmpc_request,
+                              True, ShareDataFrameStatus.EXECUTE)
+
+    @_wait_execute_decorator
+    def to_csv(self, filepath: str) -> None:
+        """計算結果をCSVに保存する．
+
+        Parameters
+        ----------
+        filepath: str
+            保存するファイルの絶対path
+
+        Returns
+        ----------
+        """
+        # 計算結果でないなら保存されないようにする
+        if not self.__is_result:
+            logger.error("Shareを保存することはできません．")
+        self.__qmpc_request.get_computation_result(self.__id, filepath)
+
+    @_wait_execute_decorator
+    def get_error_info(self) -> dict:
+        """計算中のエラー情報を取得する
+
+        Parameters
+        ----------
+
+        Returns
+        ----------
+        dict
+            なんやかんや
+        """
+        ...
