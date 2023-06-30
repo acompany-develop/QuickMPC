9872acd40905300d0d8e949e0d08fa6413bd899e
Kota Takahashi <91713065+KotaTakahashi9320@users.noreply.github.com>
2023-06-30 15:42:57 +0900

    Add benchmark_runner.sh

---
+++ benchmark_src/benchmark_runner.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+trap finally EXIT
+
+gcp_service_account_key=$1
+gcp_project_id=$2
+runner_id=$3
+channel_id=$4
+slack_token=$5
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+git fetch --tags
+latest_tag=$(git tag --list | grep -E '[0-9]+\.[0-9]+\.[0-9]+' | tail -1)
+
+function finally() {
+    # logをslackに送信
+    curl -F title="Benchmark QuickMPC==$latest_tag" -F file=@/home/ubuntu/log/result-$runner_id.log -F channels=$channel_id -H "Authorization: Bearer $slack_token" https://slack.com/api/files.upload
+    # post processing
+    rm ~/.ssh/known_hosts
+    cd $SCRIPT_DIR/../demo/terraform/gcp
+    terraform destroy --auto-approve
+}
+
+./setup.sh $latest_tag $gcp_service_account_key $gcp_project_id
+
+# Create GCP instance
+cd ../demo/terraform/gcp
+terraform init && terraform plan && terraform apply --auto-approve && terraform output -json > ../application/output.json
+
+# Deploy quickmpc
+cd ../application
+terraform init && terraform plan && terraform apply --auto-approve && terraform output -json > ../../../benchmark_src/terraform/output.json
+
+#Run benchmark test
+cd ../../../benchmark_src/terraform
+terraform init && terraform plan && terraform apply --auto-approve > /home/ubuntu/log/result-$runner_id.log
