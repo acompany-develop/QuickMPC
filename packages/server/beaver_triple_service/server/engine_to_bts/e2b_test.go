546a111f9ec16750d8a7ffe097b5eac1322ef601
Sakamoto Souta <sota.sakamoto@acompany-ac.com>
2023-07-04 04:46:57 +0000

    del request in test

--- packages/server/beaver_triple_service/server/engine_to_bts/e2b_test.go
+++ packages/server/beaver_triple_service/server/engine_to_bts/e2b_test.go
@@ -86,8 +86,6 @@ func getContext(opts ...ctxOptFunc) (context.Context, error) {
 	return ctx, nil
 }
 
-requestIdGenerator := uint32(0)
-
 func testGetTriplesByJobIdAndPartyId(t *testing.T, client pb.EngineToBtsClient, amount uint32, jobId uint32, partyId uint32) {
 	t.Run(fmt.Sprintf("testGetTriples_Party%d", partyId), func(t *testing.T) {
 		t.Helper()
@@ -98,8 +96,7 @@ func testGetTriplesByJobIdAndPartyId(t *testing.T, client pb.EngineToBtsClient, 
 			t.Fatal(err)
 		}
 
-		result, err := client.GetTriples(ctx, &pb.GetTriplesRequest{JobId: jobId, Amount: amount, TripleType: pb.Type_TYPE_FIXEDPOINT, requestId: requestIdGenerator})
-		requestIdGenerator += 1
+		result, err := client.GetTriples(ctx, &pb.GetTriplesRequest{JobId: jobId, Amount: amount, TripleType: pb.Type_TYPE_FIXEDPOINT})
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -190,8 +187,7 @@ func TestGetTriplesFailedUnknownType(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	_, err = client.GetTriples(ctx, &pb.GetTriplesRequest{JobId: 0, Amount: 1, requestIdGenerator})
-	requestIdGenerator += 1
+	_, err = client.GetTriples(ctx, &pb.GetTriplesRequest{JobId: 0, Amount: 1})
 
 	if err == nil {
 		t.Fatal("TripleTypeの指定がないRequestはエラーを出す必要があります．")
