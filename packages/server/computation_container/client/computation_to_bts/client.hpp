a92e4c8257051a12e0182a092d626cedd15b1d34
Sakamoto Souta <sota.sakamoto@acompany-ac.com>
2023-07-11 02:45:41 +0000

    change client to job base

--- packages/server/computation_container/client/computation_to_bts/client.hpp
+++ packages/server/computation_container/client/computation_to_bts/client.hpp
@@ -2,77 +2,65 @@
 
 #include <string>
 #include <vector>
-
 #include "client/helper/helper.hpp"
+#include "share/address_id.hpp"
 #include "config_parse/config_parse.hpp"
 #include "external/proto/engine_to_bts/engine_to_bts.grpc.pb.h"
 #include "logging/logger.hpp"
 
 namespace qmpc::ComputationToBts
 {
-using Triple = std::tuple<std::string, std::string, std::string>;
-
-thread_local static inline std::int64_t request_id_generator = 0;
-
 class Client
 {
 private:
     std::unique_ptr<enginetobts::EngineToBts::Stub> stub_;
+    thread_local static inline std::int64_t request_id_generator = 0;
 
     Client(const Client &) noexcept = delete;
     Client &operator=(const Client &) noexcept = delete;
     Client(Client &&) noexcept = delete;
     Client &operator=(Client &&) noexcept = delete;
 
+    template<typename T>
+    enginetobts::GetRequest makeRequest(const unsigned int amount)
+    {
+        enginetobts::GetRequest request;
+        request.set_job_id(qmpc::Share::AddressId::getThreadJobId());
+        request.set_amount(amount);
+        request.set_request_id(request_id_generator++);
+        if constexpr (std::is_same_v<T, float>)
+        {
+            request.set_type(enginetobts::Type::TYPE_FLOAT);
+        }
+        else
+        {
+            request.set_type(enginetobts::Type::TYPE_FIXEDPOINT);
+        }
+        return request;
+    }
 public:
     Client() noexcept;
     ~Client() noexcept = default;
     static std::shared_ptr<Client> getInstance();
 
-    // tripleの取り出し
-    template <class SV>
-    std::vector<Triple> readTriples(const unsigned int job_id, const unsigned int amount)
+    template<class Job>
+    std::vector<typename Job::result_type> readRequest(const unsigned int amount)
     {
-        // リクエスト設定
-        enginetobts::GetTriplesRequest request;
-        request.set_job_id(job_id);
-        request.set_amount(amount);
-        request.set_request_id(request_id_generator++);
-        if constexpr (std::is_same_v<SV, float>)
-        {
-            request.set_triple_type(enginetobts::Type::TYPE_FLOAT);
-        }
-        else
-        {
-            request.set_triple_type(enginetobts::Type::TYPE_FIXEDPOINT);
-        }
-        enginetobts::GetTriplesResponse response;
-
+        using T = typename Job::value_type;
+        auto request = makeRequest<T>(amount);
+        typename Job::response_type response;
         grpc::Status status;
 
-        // リトライポリシーに従ってリクエストを送る
-        auto retry_manager = RetryManager("BTS", "readTriples");
-
+        auto retry_manager = RetryManager("BTS", Job::op_name);
         do
         {
             grpc::ClientContext context;
             const std::string token = Config::getInstance()->cc_to_bts_token;
             context.AddMetadata("authorization", "bearer " + token);
-            status = stub_->GetTriples(&context, request, &response);
-        } while (retry_manager.retry(status));
+            status = Job::request(stub_, context, request, response);
+        }while (retry_manager.retry(status));
 
-        // responseから結果を取り出して返す
-        std::vector<Triple> triples;
-        triples.reserve(response.triples_size());
-        for (int i = 0; i < response.triples_size(); i++)
-        {
-            auto triple = response.triples(i);
-            Triple t = std::make_tuple(
-                std::to_string(triple.a()), std::to_string(triple.b()), std::to_string(triple.c())
-            );
-            triples.emplace_back(t);
-        }
-        return triples;
+        return Job::getValue(response);
     }
 };
 }  // namespace qmpc::ComputationToBts
