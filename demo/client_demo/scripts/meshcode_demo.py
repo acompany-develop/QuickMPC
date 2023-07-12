57b3b5ffff30a359000b564a5544a1fe50137ed2
Nakata <ryosuke.nakata@acompany-ac.com>
2023-07-12 18:40:52 +0900

    Fix demo

--- demo/client_demo/scripts/meshcode_demo.py
+++ demo/client_demo/scripts/meshcode_demo.py
@@ -2,6 +2,7 @@ import logging
 
 import pandas as pd
 import quickmpc
+import quickmpc.pandas as qpd
 
 if __name__ == '__main__':
     """ Step 0. loggerの設定(任意) """
@@ -19,9 +20,9 @@ if __name__ == '__main__':
 
     """ Step 2. シェア送信 """
     data_dir: str = "../data/"
-    df: pd.DataFrame = qmpc.read_csv(f"{data_dir}/data-meshcode.csv",
-                                     index_col="id")
-    sdf: quickmpc.ShareDataFrame = qmpc.send_to(df)
+    df: pd.DataFrame = qpd.read_csv(f"{data_dir}/data-meshcode.csv",
+                                    index_col="id")
+    sdf: qpd.ShareDataFrame = qmpc.send_to(df)
 
     """ Step 3. 各種統計演算 """
     logger.info(sdf.meshcode([2, 3, 4, 5]).to_data_frame())
