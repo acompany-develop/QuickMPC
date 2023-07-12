adb08699f68b3087d3c5247d2ae778ae68418a85
Sakamoto Souta <sota.sakamoto@acompany-ac.com>
2023-07-12 02:07:46 +0000

    add shellscript

---
+++ scripts/beaver_triple_service/rand_bit_store/small_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+build() {
+    docker buildx bake $COMPOSE_FILES_OPT small_bts --load
+}
+
+setup() {
+    docker-compose $COMPOSE_FILES_OPT down -v
+}
+
+run() {
+    docker-compose $COMPOSE_FILES_OPT run small_bts /bin/sh -c 'cd /QuickMPC && go mod vendor && go test -cover -v $(go list github.com/acompany-develop/QuickMPC/packages/server/beaver_triple_service/rand_bit_store/...)'
+}
+
+teardown() {
+    docker-compose $COMPOSE_FILES_OPT down -v
+}
