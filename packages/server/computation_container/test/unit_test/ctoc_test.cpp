bb61062ac1ed8c5ba17d94614fd0e771e85cd42a
Sakamoto Souta <sota.sakamoto@acompany-ac.com>
2023-07-27 05:21:26 +0000

    cancel comment out

--- packages/server/computation_container/test/unit_test/ctoc_test.cpp
+++ packages/server/computation_container/test/unit_test/ctoc_test.cpp
@@ -17,126 +17,126 @@
 #include "server/computation_to_computation_container/server.hpp"
 #include "share/address_id.hpp"
 
-// void runServer(std::string endpoint)
-// {
-//     auto server = qmpc::ComputationToComputation::Server::getServer();
-//     grpc::ServerBuilder builder;
-//     /*
-//     grpc Server keepalive設定
-//     GRPC_ARG_HTTP2_MIN_RECV_PING_INTERVAL_WITHOUT_DATA_MS　pingの間隔がこの時間より短い場合は攻撃とみなす　9秒
-//     （この時間はクライアントと調整する）
-//     GRPC_ARG_KEEPALIVE_PERMIT_WITHOUT_CALLS　通信がなくてもpingを送る　on
-//     */
-//     builder.AddChannelArgument(GRPC_ARG_HTTP2_MIN_RECV_PING_INTERVAL_WITHOUT_DATA_MS, 9000);
-//     builder.AddChannelArgument(GRPC_ARG_KEEPALIVE_PERMIT_WITHOUT_CALLS, 1);
-//     builder.AddListeningPort(endpoint, grpc::InsecureServerCredentials());
-//     builder.RegisterService(server);
-//     std::unique_ptr<grpc::Server> listener(builder.BuildAndStart());
-//     QMPC_LOG_INFO("[CcToCc_test] Server listening on {}", endpoint);
-//     listener->Wait();
-// }
+void runServer(std::string endpoint)
+{
+    auto server = qmpc::ComputationToComputation::Server::getServer();
+    grpc::ServerBuilder builder;
+    /*
+    grpc Server keepalive設定
+    GRPC_ARG_HTTP2_MIN_RECV_PING_INTERVAL_WITHOUT_DATA_MS　pingの間隔がこの時間より短い場合は攻撃とみなす　9秒
+    （この時間はクライアントと調整する）
+    GRPC_ARG_KEEPALIVE_PERMIT_WITHOUT_CALLS　通信がなくてもpingを送る　on
+    */
+    builder.AddChannelArgument(GRPC_ARG_HTTP2_MIN_RECV_PING_INTERVAL_WITHOUT_DATA_MS, 9000);
+    builder.AddChannelArgument(GRPC_ARG_KEEPALIVE_PERMIT_WITHOUT_CALLS, 1);
+    builder.AddListeningPort(endpoint, grpc::InsecureServerCredentials());
+    builder.RegisterService(server);
+    std::unique_ptr<grpc::Server> listener(builder.BuildAndStart());
+    QMPC_LOG_INFO("[CcToCc_test] Server listening on {}", endpoint);
+    listener->Wait();
+}
 
-// TEST(CtoC_Test, EXCHANGESHARE)
-// {
-//     Config *conf = Config::getInstance();
-//     std::vector<qmpc::Share::AddressId> share_id(1);
-//     computationtocomputation::Shares share;
-//     std::cout << "exchangeshare id " << share_id[0] << std::endl;
-//     std::string value = "10";
-//     auto a = share.mutable_address_id();
-//     a->set_share_id(share_id[0].getShareId());
-//     a->set_job_id(share_id[0].getJobId());
-//     a->set_party_id(conf->party_id);
-//     computationtocomputation::Shares_Share *multiple_shares = share.add_share_list();
-//     multiple_shares->set_byte(value);
+TEST(CtoC_Test, EXCHANGESHARE)
+{
+    Config *conf = Config::getInstance();
+    std::vector<qmpc::Share::AddressId> share_id(1);
+    computationtocomputation::Shares share;
+    std::cout << "exchangeshare id " << share_id[0] << std::endl;
+    std::string value = "10";
+    auto a = share.mutable_address_id();
+    a->set_share_id(share_id[0].getShareId());
+    a->set_job_id(share_id[0].getJobId());
+    a->set_party_id(conf->party_id);
+    computationtocomputation::Shares_Share *multiple_shares = share.add_share_list();
+    multiple_shares->set_byte(value);
 
-//     auto stub_ = createStub<computationtocomputation::ComputationToComputation>(
-//         Url::Parse("http://localhost:50120")
-//     );
-//     grpc::ClientContext context;
-//     google::protobuf::Empty response;
-//     std::shared_ptr<grpc::ClientWriter<computationtocomputation::Shares>> stream(
-//         stub_->ExchangeShares(&context, &response)
-//     );
-//     stream->Write(share);
-//     stream->WritesDone();
-//     grpc::Status status = stream->Finish();
-//     EXPECT_TRUE(status.ok());
+    auto stub_ = createStub<computationtocomputation::ComputationToComputation>(
+        Url::Parse("http://localhost:50120")
+    );
+    grpc::ClientContext context;
+    google::protobuf::Empty response;
+    std::shared_ptr<grpc::ClientWriter<computationtocomputation::Shares>> stream(
+        stub_->ExchangeShares(&context, &response)
+    );
+    stream->Write(share);
+    stream->WritesDone();
+    grpc::Status status = stream->Finish();
+    EXPECT_TRUE(status.ok());
 
-//     auto server = qmpc::ComputationToComputation::Server::getServer();
-//     auto data = server->getShare(conf->party_id, share_id[0]);
-//     EXPECT_EQ(value, data);
-// }
-// TEST(CtoC_Test, EXCHANGESHARES)
-// {
-//     Config *conf = Config::getInstance();
-//     unsigned int length = 2;
-//     std::vector<qmpc::Share::AddressId> share_ids(length);
-//     std::vector<std::string> values = {"10", "11"};
-//     computationtocomputation::Shares shares;
-//     auto a = shares.mutable_address_id();
-//     a->set_share_id(share_ids[0].getShareId());
-//     a->set_job_id(share_ids[0].getJobId());
-//     a->set_party_id(conf->party_id);
-//     for (unsigned int i = 0; i < length; i++)
-//     {
-//         computationtocomputation::Shares_Share *multiple_shares = shares.add_share_list();
-//         multiple_shares->set_byte(values[i]);
-//     }
-//     auto stub_ = createStub<computationtocomputation::ComputationToComputation>(
-//         Url::Parse("http://localhost:50120")
-//     );
-//     grpc::ClientContext context;
-//     google::protobuf::Empty response;
-//     std::shared_ptr<grpc::ClientWriter<computationtocomputation::Shares>> stream(
-//         stub_->ExchangeShares(&context, &response)
-//     );
-//     stream->Write(shares);
-//     stream->WritesDone();
-//     grpc::Status status = stream->Finish();
-//     EXPECT_TRUE(status.ok());
+    auto server = qmpc::ComputationToComputation::Server::getServer();
+    std::string data = server->getShare<std::string>(conf->party_id, share_id[0]);
+    EXPECT_EQ(value, data);
+}
+TEST(CtoC_Test, EXCHANGESHARES)
+{
+    Config *conf = Config::getInstance();
+    unsigned int length = 2;
+    std::vector<qmpc::Share::AddressId> share_ids(length);
+    std::vector<std::string> values = {"10", "11"};
+    computationtocomputation::Shares shares;
+    auto a = shares.mutable_address_id();
+    a->set_share_id(share_ids[0].getShareId());
+    a->set_job_id(share_ids[0].getJobId());
+    a->set_party_id(conf->party_id);
+    for (unsigned int i = 0; i < length; i++)
+    {
+        computationtocomputation::Shares_Share *multiple_shares = shares.add_share_list();
+        multiple_shares->set_byte(values[i]);
+    }
+    auto stub_ = createStub<computationtocomputation::ComputationToComputation>(
+        Url::Parse("http://localhost:50120")
+    );
+    grpc::ClientContext context;
+    google::protobuf::Empty response;
+    std::shared_ptr<grpc::ClientWriter<computationtocomputation::Shares>> stream(
+        stub_->ExchangeShares(&context, &response)
+    );
+    stream->Write(shares);
+    stream->WritesDone();
+    grpc::Status status = stream->Finish();
+    EXPECT_TRUE(status.ok());
 
-//     auto server = qmpc::ComputationToComputation::Server::getServer();
-//     auto datas = server->getShares(conf->party_id, share_ids);
-//     for (unsigned int i = 0; i < length; i++)
-//     {
-//         EXPECT_EQ(values[i], datas[i]);
-//     }
-// }
+    auto server = qmpc::ComputationToComputation::Server::getServer();
+    std::string datas = server->getShares<std::string>(conf->party_id, share_ids);
+    for (unsigned int i = 0; i < length; i++)
+    {
+        EXPECT_EQ(values[i], datas[i]);
+    }
+}
 
-// TEST(CtoC_Test, GetShareThrowExceptionTest)
-// {
-//     Config *conf = Config::getInstance();
-//     qmpc::Share::AddressId share_id;
+TEST(CtoC_Test, GetShareThrowExceptionTest)
+{
+    Config *conf = Config::getInstance();
+    qmpc::Share::AddressId share_id;
 
-//     auto server = qmpc::ComputationToComputation::Server::getServer();
-//     EXPECT_ANY_THROW(server->getShare(conf->party_id, share_id));
-// }
+    auto server = qmpc::ComputationToComputation::Server::getServer();
+    EXPECT_ANY_THROW(server->getShare<std::string>(conf->party_id, share_id));
+}
 
-// TEST(CtoC_Test, GetSharesThrowExceptionTest)
-// {
-//     Config *conf = Config::getInstance();
-//     unsigned int length = 1;
-//     std::vector<qmpc::Share::AddressId> share_ids(length);
+TEST(CtoC_Test, GetSharesThrowExceptionTest)
+{
+    Config *conf = Config::getInstance();
+    unsigned int length = 1;
+    std::vector<qmpc::Share::AddressId> share_ids(length);
 
-//     auto server = qmpc::ComputationToComputation::Server::getServer();
-//     EXPECT_ANY_THROW(server->getShares(conf->party_id, share_ids));
-// }
-// int main(int argc, char **argv)
-// {
-//     Config *conf = Config::getInstance();
+    auto server = qmpc::ComputationToComputation::Server::getServer();
+    EXPECT_ANY_THROW(server->getShares<std::string>(conf->party_id, share_ids));
+}
+int main(int argc, char **argv)
+{
+    Config *conf = Config::getInstance();
 
-//     Url ctoc_ip = conf->ip_addr_map[conf->party_id];
-//     const std::string ctc_my_ip_str("0.0.0.0:" + ctoc_ip.port);
-//     std::thread th1(runServer, ctc_my_ip_str);
-//     sleep(3);
+    Url ctoc_ip = conf->ip_addr_map[conf->party_id];
+    const std::string ctc_my_ip_str("0.0.0.0:" + ctoc_ip.port);
+    std::thread th1(runServer, ctc_my_ip_str);
+    sleep(3);
 
-//     ::testing::InitGoogleTest(&argc, argv);
-//     int ok = RUN_ALL_TESTS();
-//     if (ok == 0)
-//         std::exit(EXIT_SUCCESS);
-//     else
-//         std::exit(EXIT_FAILURE);
+    ::testing::InitGoogleTest(&argc, argv);
+    int ok = RUN_ALL_TESTS();
+    if (ok == 0)
+        std::exit(EXIT_SUCCESS);
+    else
+        std::exit(EXIT_FAILURE);
 
-//     th1.detach();
-// }
+    th1.detach();
+}
