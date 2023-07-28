87abbd0ed24c782b8f6490997ce96364884c4d57
Sakamoto Souta <sota.sakamoto@acompany-ac.com>
2023-07-28 07:03:33 +0000

    fix math

--- packages/server/computation_container/math/math.cpp
+++ packages/server/computation_container/math/math.cpp
@@ -5,35 +5,62 @@ namespace qmpc::Math
 {
 Share sum(const std::vector<Share> &v)
 {
-    Share e(FixedPoint(0));
-    return std::accumulate(v.begin(), v.end(), e);
+    Share ret;
+    for (const auto &a : v)
+    {
+        ret += a;
+    }
+    return ret;
 }
 Share smean(const std::vector<Share> &v)
 {
-    Share ret = sum(v);
+    // Share avg(FixedPoint(0.0));
+    // for (int i = 0; i < size; i++)
+    // {
+    //     avg = avg + v[i];
+    // }
+    Share ret{};
+    for (auto a : v)
+    {
+        ret += a;
+    }
     int size = std::size(v);
     ret /= FixedPoint(size);
 
     return ret;
 }
 
-Share variance(const std::vector<Share> &v)
+Share variance(std::vector<Share> &v)
 {
-    Share avg = smean(v);
+    Share avg;
+
+    avg = smean(v);
     std::vector<Share> var;
-    std::transform(
-        v.begin(), v.end(), std::back_inserter(var), [&avg](const Share &s) { return s - avg; }
-    );
+    for (auto &a : v)
+    {
+        var.emplace_back(a - avg);
+    }
     auto varVec = var * var;
-    return smean(varVec);
+    Share ret{};
+    for (auto &a : varVec)
+    {
+        ret += a;
+    }
+    // FPのresolutionの制約により、FPだと1000より大きい数で割れないためdoubleで割っている
+    // double var_d = var.getDoubleVal();
+    int size = std::size(v);
+    ret /= FixedPoint(size);
+
+    return ret;
 }
 
-FixedPoint stdev(const std::vector<Share> &v)
+FixedPoint stdev(std::vector<Share> &v)
 {
-    Share var = variance(v);
-    FixedPoint var_val = open_and_recons(var);
+    Share var;
+    var = variance(v);
 
-    auto value = var_val.getDoubleVal<boost::multiprecision::cpp_dec_float_100>();
+    FixedPoint stdev = open_and_recons(var);
+    auto value = boost::multiprecision::cpp_dec_float_100(stdev.getStrVal());
     if (value < 0)
     {
         value = 0;
@@ -42,35 +69,53 @@ FixedPoint stdev(const std::vector<Share> &v)
     FixedPoint ret{r};
     return ret;
 }
-Share correl(const std::vector<Share> &x, const std::vector<Share> &y)
+Share correl(std::vector<Share> &x, std::vector<Share> &y)
 {
-    size_t n = x.size();
+    int sizex = (int)x.size();
+    int sizey = (int)y.size();
 
-    if (n != y.size())
+    if (sizex != sizey)
     {
         qmpc::Log::throw_with_trace(std::runtime_error("input Size is not Equal"));
     }
+    Share aveX = smean(x);
+    Share aveY = smean(y);
 
     FixedPoint stdeX = stdev(x);
     FixedPoint stdeY = stdev(y);
 
-    // 0除算
-    if (stdeX == FixedPoint(0) || stdeY == FixedPoint(0))
+    // 0徐算エラー
+    // TODO:一時的に値を入れているので要修正
+    if (stdeX <= FixedPoint(0.001) and stdeX >= FixedPoint(-0.001))
     {
-        qmpc::Log::throw_with_trace(std::runtime_error("Div0"));
+        stdeX = FixedPoint("0.001");
     }
 
-    Share aveX = smean(x);
-    Share aveY = smean(y);
-    std::vector<Share> tmpX(n);
-    std::vector<Share> tmpY(n);
-    for (size_t i = 0; i < n; ++i)
+    if (stdeY <= FixedPoint(0.001) and stdeY >= FixedPoint(-0.001))
     {
-        tmpX[i] = x[i] - aveX;
-        tmpY[i] = y[i] - aveY;
+        stdeY = FixedPoint("0.001");
+    }
+
+    // if (stdeX.getVal() == 0 or stdeY.getVal() == 0)
+    // {
+    //     throw std::runtime_error("0 divede error");
+    // }
+    int n = sizex;
+    std::vector<Share> tmpX;
+    tmpX.reserve(n);
+    std::vector<Share> tmpY;
+    tmpY.reserve(n);
+    for (int i = 0; i < n; ++i)
+    {
+        tmpX.emplace_back(x[i] - aveX);
+        tmpY.emplace_back(y[i] - aveY);
     }
     auto tmpVec = tmpX * tmpY;
-    Share ret = sum(tmpVec);
+    Share ret{};
+    for (auto &r : tmpVec)
+    {
+        ret += r;
+    }
     ret /= stdeX;
     ret /= stdeY;
     ret /= FixedPoint(n);
