471521c68356c7c844dedec04bb8aa9814422e9f
Sakamoto Souta <sota.sakamoto@acompany-ac.com>
2023-07-31 02:55:36 +0000

    refactoring math

--- packages/server/computation_container/math/math.cpp
+++ packages/server/computation_container/math/math.cpp
@@ -5,111 +5,68 @@ namespace qmpc::Math
 {
 Share sum(const std::vector<Share> &v)
 {
-    Share ret;
-    for (const auto &a : v)
-    {
-        ret += a;
-    }
-    return ret;
+    Share e(FixedPoint(0));
+    return std::accumulate(v.begin(), v.end(), e);
 }
-Share smean(const std::vector<Share> &v)
-{
-    // Share avg(FixedPoint(0.0));
-    // for (int i = 0; i < size; i++)
-    // {
-    //     avg = avg + v[i];
-    // }
-    Share ret{};
-    for (auto a : v)
-    {
-        ret += a;
-    }
-    int size = std::size(v);
-    ret /= FixedPoint(size);
+Share smean(const std::vector<Share> &v) { return sum(v) / FixedPoint(std::size(v)); }
 
-    return ret;
+std::vector<Share> deviation(std::vector<Share> v)
+{
+    Share avg = smean(v);
+    for (auto &s : v)
+    {
+        s -= avg;
+    }
+    return v;
 }
 
-Share variance(std::vector<Share> &v)
+Share variance(const std::vector<Share> &v)
 {
-    Share avg;
-
-    avg = smean(v);
-    std::vector<Share> var;
-    for (auto &a : v)
-    {
-        var.emplace_back(a - avg);
-    }
-    auto varVec = var * var;
-    Share ret{};
-    for (auto &a : varVec)
-    {
-        ret += a;
-    }
-    // FPのresolutionの制約により、FPだと1000より大きい数で割れないためdoubleで割っている
-    // double var_d = var.getDoubleVal();
-    int size = std::size(v);
-    ret /= FixedPoint(size);
-
-    return ret;
+    auto dev = deviation(v);
+    auto var = dev * dev;
+    return smean(var);
 }
 
-FixedPoint stdev(std::vector<Share> &v)
+FixedPoint stdev(const std::vector<Share> &v)
 {
-    Share var;
-    var = variance(v);
+    Share var = variance(v);
+    FixedPoint var_val = open_and_recons(var);
 
-    FixedPoint stdev = open_and_recons(var);
-    auto value = stdev.getDoubleVal<qmpc::Utils::mp_float>();
+    auto value = var_val.getDoubleVal<qmpc::Utils::mp_float>();
     if (value < 0)
     {
         value = 0;
     }
-    auto r = sqrt(value);
-    FixedPoint ret{r};
-    return ret;
+    return FixedPoint(sqrt(value));
 }
-Share correl(std::vector<Share> &x, std::vector<Share> &y)
-{
-    int sizex = (int)x.size();
-    int sizey = (int)y.size();
 
-    if (sizex != sizey)
+Share covariance(const std::vector<Share> &x, const std::vector<Share> &y)
+{
+    auto devX = deviation(x);
+    auto devY = deviation(y);
+    auto devXY = devX * devY;
+    return smean(devXY);
+}
+
+Share correl(const std::vector<Share> &x, const std::vector<Share> &y)
+{
+    size_t n = x.size();
+
+    if (n != y.size())
     {
         qmpc::Log::throw_with_trace(std::runtime_error("input Size is not Equal"));
     }
-    Share aveX = smean(x);
-    Share aveY = smean(y);
 
     FixedPoint stdeX = stdev(x);
     FixedPoint stdeY = stdev(y);
 
+    // 0除算
     if (stdeX == FixedPoint(0) || stdeY == FixedPoint(0))
     {
-        QMPC_LOG_ERROR("correl returns 0 when stdev is 0");
-        return Share(FixedPoint(0));
+        qmpc::Log::throw_with_trace(std::runtime_error("correl Div0 error"));
     }
 
-    int n = sizex;
-    std::vector<Share> tmpX;
-    tmpX.reserve(n);
-    std::vector<Share> tmpY;
-    tmpY.reserve(n);
-    for (int i = 0; i < n; ++i)
-    {
-        tmpX.emplace_back(x[i] - aveX);
-        tmpY.emplace_back(y[i] - aveY);
-    }
-    auto tmpVec = tmpX * tmpY;
-    Share ret{};
-    for (auto &r : tmpVec)
-    {
-        ret += r;
-    }
-    ret /= stdeX;
-    ret /= stdeY;
-    ret /= FixedPoint(n);
-    return ret;
+    return covariance(x, y) / (stdeX * stdeY);
 }
 
 Share exp(const Share &x)
