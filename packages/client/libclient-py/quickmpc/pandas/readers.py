e1090f1f09d33700e8789c46a044a6ee3e67aa56
Nakata <ryosuke.nakata@acompany-ac.com>
2023-08-01 21:59:41 +0900

    Update read_csv doc to add pandas doc

--- packages/client/libclient-py/quickmpc/pandas/readers.py
+++ packages/client/libclient-py/quickmpc/pandas/readers.py
@@ -1,26 +1,32 @@
 import pandas as pd
+from pandas.core.shared_docs import _shared_docs
+from pandas.io.parsers.readers import _doc_read_csv_and_table
+from pandas.util._decorators import Appender
 
 from quickmpc.pandas.parser import to_float
 
 
+# pandas.read_csvからdocumentを持ってくる
+@Appender(
+    _doc_read_csv_and_table.format(
+        func_name="read_csv",
+        summary="Read a comma-separated values (csv) file into DataFrame.",
+        _default_sep="','",
+        storage_options=_shared_docs["storage_options"],
+        decompression_options="",  # TODO: 取得できなかったので原因を特定する
+        # decompression_options=_shared_docs["decompression_options"]
+        # % "filepath_or_buffer",
+    )
+)
 def read_csv(*args, index_col: str, **kwargs) -> pd.DataFrame:
-    """csvからテーブルデータを読み込む．
+    """csvからテーブルデータを読み込む
 
-    テーブル結合処理に用いる列がどの列かを`index_col`で指定する必要がある．
-    `index_col`以外の引数は全てpandasのread_csvと同じ．
+    テーブル結合処理に用いる列がどの列かを `index_col` で列名を指定する必要がある．
+    `index_col` 以外の引数は全てpandasのread_csvと同じ．
+    以下のdocumentは `pandasのdocument <https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html?highlight=read_csv>`_ より．
 
-    Parameters
-    ----------
-    filepath_or_buffer: FilePath | ReadCsvBuffer[bytes] | ReadCsvBuffer[str],
-        らしい
-    index_col: str
-        ID列としたいカラム名
-
-    Returns
-    ----------
-    pd.DataFrame
-        読み込んだテーブルデータ
-    """
+    .. docにはpandasからコピーしたdocumentが表示される
+    """  # noqa: E501
     df = pd.read_csv(*args, **kwargs)
     # ID列を数値化
     df[index_col] = df[index_col].map(lambda x: to_float(x))
