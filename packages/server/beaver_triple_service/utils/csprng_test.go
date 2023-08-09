483ff13fc25419b2b7a1e7fb3c64a71eadc7f0e0
Sakamoto Souta <sota.sakamoto@acompany-ac.com>
2023-08-09 02:06:23 +0000

    add TestGetRandBigInts

--- packages/server/beaver_triple_service/utils/csprng_test.go
+++ packages/server/beaver_triple_service/utils/csprng_test.go
@@ -6,10 +6,39 @@ import (
 	utils "github.com/acompany-develop/QuickMPC/packages/server/beaver_triple_service/utils"
 )
 
+func TestGetRandBigInts(t *testing.T){
+	t.Helper()
+	rands, err := utils.GetRandBigInts(90, 100000)
+	if err != nil{
+		t.Fatal(err)
+	}
+
+	x := big.NewInt(0)
+
+	for _, rand := range rands {
+		sgn := rand.Sign()
+		if sgn < 0 {
+			err := errors.New("negative rand")
+			t.Fatal(err)
+		}
+		x.Or(x, rand)
+	}
+
+	expected := big.NewInt(1)
+	expected.Lsh(expected, 96)
+	expected.Sub(expected, 1)
+
+	diff := x.Cmp(expected)
+	if diff != 0 {
+		err := errors.New("missing bit")
+		t.Fatal(err)
+	}
+}
+
 var randMax = int64(10000000)
 var randMin = int64(-10000000)
 
-func testCsprng(t *testing.T, sliceSize uint64, randMin int64, randMax int64) {
+func testGetRandInt64Slice(t *testing.T, sliceSize uint64, randMin int64, randMax int64) {
 	t.Helper()
 	randSlices, err := utils.GetRandInt64Slice(sliceSize, randMin, randMax)
 	if err != nil {
@@ -28,12 +57,12 @@ func testCsprng(t *testing.T, sliceSize uint64, randMin int64, randMax int64) {
 	}
 }
 
-func TestCsprng_1(t *testing.T)       { testCsprng(t, 1, randMin, randMax) }
-func TestCsprng_100(t *testing.T)     { testCsprng(t, 100, randMin, randMax) }
-func TestCsprng_10000(t *testing.T)   { testCsprng(t, 10000, randMin, randMax) }
-func TestCsprng_1000000(t *testing.T) { testCsprng(t, 1000000, randMin, randMax) }
+func TestGetRandInt64Slice_1(t *testing.T)       { testGetRandInt64Slice(t, 1, randMin, randMax) }
+func TestGetRandInt64Slice_100(t *testing.T)     { testGetRandInt64Slice(t, 100, randMin, randMax) }
+func TestGetRandInt64Slice_10000(t *testing.T)   { testGetRandInt64Slice(t, 10000, randMin, randMax) }
+func TestGetRandInt64Slice_1000000(t *testing.T) { testGetRandInt64Slice(t, 1000000, randMin, randMax) }
 
-func TestCsprng_CornerCase_1(t *testing.T) {
+func TestGetRandInt64Slice_CornerCase_1(t *testing.T) {
 	// sliceSize = 1, randMin == randMaxのケース
 	sliceSize := uint64(1)
 	randMin := int64(1)
@@ -57,7 +86,7 @@ func TestCsprng_CornerCase_1(t *testing.T) {
 	}
 }
 
-func TestCsprng_CornerCase_2(t *testing.T) {
+func TestGetRandInt64Slice_CornerCase_2(t *testing.T) {
 	// 閉区間で生成できているかの確認
 	sliceSize := uint64(1000)
 	randMin := int64(-1)
